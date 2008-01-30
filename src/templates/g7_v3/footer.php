3370838b74838f9e68999dc919b0b6b4d67a70fd
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-01-30 14:55:18 +0000

    修改g7系列模板的版权URL地址。
    git-svn-id: http://emlog.googlecode.com/svn/trunk@173 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/templates/g7_v3/footer.php
+++ src/templates/g7_v3/footer.php
@@ -3,7 +3,7 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 print <<<EOT
 -->
 </div><div id="footer">
-&copy; 2007 <a href="http://www.emlog.net" target="_blank">emlog</a> <a href="http://www.miibeian.gov.cn" target="_blank">$icp</a> Theme produced by <a title="Theme by underone" onclick="parent.location.href='http://redirect.alexa.com/redirect?http://www.underone.com/';return false;" href="http://www.underone.com/">underone</a>
+&copy; 2007 <a href="http://www.emlog.net" target="_blank">emlog</a> <a href="http://www.miibeian.gov.cn" target="_blank">$icp</a> Theme produced by <a href="http://www.underone.com/">underone</a>
 </div>
 </body>
 </html>
