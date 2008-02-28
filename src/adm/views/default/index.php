b484e306395aa59766f5c6df8397cbcf4f68af2c
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-02-28 14:06:43 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@227 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/adm/views/default/index.php
+++ src/adm/views/default/index.php
@@ -41,15 +41,14 @@ EOT;
 		<td><a href="configure.php?action=phpinfo">[phpinfo]</a></td>
 	</tr>
 </table>
-<div class=containertitle><b>关于我们</b></div>
-<div class=line></div>
+<div class=line2></div>
 <table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
 	<tr>
-		<td width="50%">程序设计: <a href="http://www.emlog.net/blog">那多记忆</a></td>
-		<td width="50%">美工: <a href="mailto:jflyang@sohu.com">CrazyDavinc</a></td>
+		<td width="50%">官方主页: <a href="http://www.emlog.net" target="_blank">www.emlog.net</a></font></td>
+		<td width="50%">官方博客: <a href="http://www.emlog.net/blog" target="_blank">every memory log</a></td>
 	</tr>
 	<tr>
-		<td width="50%">官方主页: <a href="http://www.emlog.net" target="_blank">Www.Emlog.Net</a></font></td>
+		<td width="50%">工程控制: <a href="http://code.google.com/p/emlog/" target="_blank">google-code.emlog</a></td>
 		<td width="50%">电子邮件: <a href="mailto:emloog@gmail.com">emloog@gmail.com</a></td>
 	</tr>
 </table>
