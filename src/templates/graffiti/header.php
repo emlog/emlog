850ca6b6b2cc034c5b46fda391ad77393ae6e6e5
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-08-04 15:17:03 +0000

    修改模板
    git-svn-id: http://emlog.googlecode.com/svn/trunk@606 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

---
+++ src/templates/graffiti/header.php
@@ -0,0 +1,51 @@
+<?php
+if(!defined('EMLOG_ROOT')) {exit('error!');}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Language" content="zh-CN" />
+<meta name="description" content="<?php echo $sitekey; ?>" />
+<meta name="keywords" content="emlog,blog,<?php echo $sitekey; ?>" />
+<meta name="copyright" content="emlog" />
+<meta name="author" content="emlog" />
+<title><?php echo $blogtitle; ?></title>
+<link rel="alternate" type="application/rss+xml" title="订阅我的博客"  href="./rss.php">
+<link href="<?php echo $tpl_dir; ?>graffiti/graf.css" rel="stylesheet" type="text/css" />
+<script src="./lib/js/common_tpl.js" type="text/javascript"></script>
+</head>
+<body>
+<div id="holder">
+<div id="header">
+	<div id="siteTitle">
+	<ul>
+  		<li id="blogname"><a href="./"><?php echo $blogname; ?></a></li>
+  		<li id="blogdes"><?php echo $bloginfo; ?></li>
+	</ul>
+  	</div>
+<form name="f" method="post" action="index.php?action=login">
+<div id="navBar">
+<ul>
+<?php if(ISLOGIN): ?>
+	<li><a href="./adm/add_log.php">写日志</a></li>
+	<li><a href="./adm/">管理中心</a></li>
+	<li><a href="./index.php?action=logout">退出</a></li>
+<?php
+else:
+$login_code=='y'?
+$ckcode= "<img src=\"./lib/C_checkcode.php\" align=\"absmiddle\"><input name=\"imgcode\" type=\"text\" class=\"input\" style=\"width:40px;\">":
+$ckcode = '';
+?>
+<li onclick="showhidediv('loginfm','user')" style="cursor:pointer;">登录</li>
+<li id="loginfm" style="display:none">
+用户:<input name="user" type="text" class="input" style="width:80px;"/>
+密码:<input name="pw" type="password"  class="input" style="width:80px;"/>
+<?php echo $ckcode; ?> 
+<input type="submit" value="登录">
+</li>
+<?php endif; ?>
+</ul>
+</div>
+</form>
+</div>
---
+++ src/templates/graffiti/log_list.php
@@ -0,0 +1,28 @@
+<?php 
+if(!defined('EMLOG_ROOT')) {exit('error!');}
+include getViews('side');
+foreach($logs as $value):
+?>
+
+<div class="logcontent">
+<div id="t">
+<?php echo $value['toplog']; ?><a href="./?action=showlog&gid=<?php echo $value['logid']; ?>"><?php echo $value['log_title']; ?></a>
+</div>
+
+<p id="date"><?php echo $value['post_time']; ?></p>
+<div class="log_desc"><?php echo $value['log_description']; ?></div>
+<p><?php echo $value['att_img']; ?></p>
+<p><?php echo $value['attachment']; ?></p>
+<p><?php echo $value['tag']; ?></p>
+
+<div align="right">
+<a href="./?action=showlog&gid=<?php echo $value['logid']; ?>#comment">评论(<?php echo $value['comnum']; ?>)</a>
+<a href="./?action=showlog&gid=<?php echo $value['logid']; ?>#tb">引用(<?php echo $value['tbcount']; ?>)</a> 
+<a href="./?action=showlog&gid=<?php echo $value['logid']; ?>">浏览(<?php echo $value['views']; ?>)</a>
+</div>
+</div>
+<?php endforeach;  ?>
+
+<div id="pageurl"><?php echo $page_url;?></div>
+
+<?php include getViews('footer'); ?>
