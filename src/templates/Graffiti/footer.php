05cd2228d20bdeb883abe93c518354958131fbd5
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-08-04 15:07:46 +0000

    修改模板
    git-svn-id: http://emlog.googlecode.com/svn/trunk@605 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/templates/Graffiti/footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');} ?>
-</div>
-</div>
-</div>
-<div id="siteCopyright">Powered by 
-<a href="http://www.emlog.net" title="emlog <?php echo $edition;?>">emlog</a>
-<a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a>
-</body>
-</html>
