c7d6364be72bcd3993d2896cc827c085d884a6bf
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-10-13 15:41:37 +0000

    改变wap的访问路径，删除了wap文件夹
    git-svn-id: http://emlog.googlecode.com/svn/trunk@674 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/wap.php
+++ src/wap.php
@@ -3,10 +3,10 @@
  * 手机 wap 第一版
  * @copyright (c) 2008, Emlog All Rights Reserved
  * @version emlog-2.7.0
- * $Id$
+ * $Id: wap.php 526 2008-07-05 15:21:03Z emloog $
  */
 
-require_once('../common.php');
+require_once('./common.php');
 
 define('CURPAGE','wap');
 
@@ -15,17 +15,17 @@ if(!isset($action) || empty($action))
 	wap_header($config_cache['blogname']);
 	echo '<p>'.$config_cache['bloginfo'].'</p>';
 	echo "<p>\n";
-	echo "<a href=\"index.php?action=logs\">浏览日志</a><br />\n";
-	echo "<a href=\"index.php?action=twitter\">博主唠叨</a><br />\n";
-	echo "<a href=\"index.php?action=coms\">最新评论</a><br />\n";
+	echo "<a href=\"wap.php?action=logs\">浏览日志</a><br />\n";
+	echo "<a href=\"wap.php?action=twitter\">博主唠叨</a><br />\n";
+	echo "<a href=\"wap.php?action=coms\">最新评论</a><br />\n";
 	echo "<br />\n";
 	if(ISLOGIN === true)
 	{
 		echo "欢迎你,你已登录<br />\n";
-		echo "<a href=\"index.php?action=addtw\">唠叨两句</a><br />\n";
-		echo "<a href=\"index.php?action=logout\">退出</a><br />\n";
+		echo "<a href=\"wap.php?action=addtw\">唠叨两句</a><br />\n";
+		echo "<a href=\"wap.php?action=logout\">退出</a><br />\n";
 	}else {
-		echo "<a href=\"index.php?action=waplogin\">登录</a><br />\n";
+		echo "<a href=\"wap.php?action=waplogin\">登录</a><br />\n";
 	}
 	echo "<br />\n";
 	echo "日志({$sta_cache['lognum']})评论({$sta_cache['comnum']})引用({$sta_cache['tbnum']})<br />今日访问({$sta_cache['day_view_count']})总访问量({$sta_cache['view_count']})<br />\n";
@@ -52,7 +52,7 @@ if ($action == 'logs')
 
 	$sql =" SELECT * FROM {$db_prefix}blog WHERE hide='n' ORDER BY top DESC ,date DESC  LIMIT $start_limit, $index_lognum";
 	$lognum = $sta_cache['lognum'];
-	$pageurl= './index.php?action=logs&amp;page';
+	$pageurl= './wap.php?action=logs&amp;page';
 	$query = $DB->query($sql);
 	while($row = $DB->fetch_array($query))
 	{
@@ -70,13 +70,13 @@ if ($action == 'logs')
 	{
 		foreach ($log as $val)
 		{
-			echo '<a href="./index.php?action=dis&amp;id='.$val['logid'].'">'.$val['log_title'].'</a>('.$val['views'].'/'.$val['comnum'].')<br />';
+			echo '<a href="./wap.php?action=dis&amp;id='.$val['logid'].'">'.$val['log_title'].'</a>('.$val['views'].'/'.$val['comnum'].')<br />';
 		}
 	}else
 	{
 		echo 'No logs yet!';
 	}
-	echo "</p><p>$page_url <br /><a href=\"./index.php\">首页</a></p>";
+	echo "</p><p>$page_url <br /><a href=\"./wap.php\">首页</a></p>";
 	wap_footer();
 }
 
@@ -84,9 +84,9 @@ if ($action == 'logs')
 if ($action == 'dis')
 {
 	//参数过滤
-	isset($_GET['id']) ? $logid = intval($_GET['id']) : msg('提交参数错误','./index.php');
+	isset($_GET['id']) ? $logid = intval($_GET['id']) : msg('提交参数错误','./wap.php');
 	$show_log = @$DB->fetch_one_array("SELECT * FROM {$db_prefix}blog WHERE gid='$logid' AND hide='n' ")
-	OR msg('不存在该日志','./index.php');
+	OR msg('不存在该日志','./wap.php');
 	$DB->query("UPDATE {$db_prefix}blog SET views=views+1 WHERE gid='".$show_log['gid']."'");
 
 	$log_title  = htmlspecialchars($show_log['title']);
@@ -99,7 +99,7 @@ if ($action == 'dis')
 	echo "<p>发布时间：$post_time <br /></p>";
 	echo "<p>$log_content</p>";
 	$stamp = time();
-	echo "<p><a href=\"./index.php?stamp=$stamp\">首页</a> <a href=\"./?action=logs\">返回日志列表</a></p>";
+	echo "<p><a href=\"./wap.php?stamp=$stamp\">首页</a> <a href=\"./?action=logs\">返回日志列表</a></p>";
 
 	wap_footer();
 }
@@ -116,7 +116,7 @@ if($action == 'coms')
 	{
 		echo 'No comments yet!';
 	}
-	echo "<p><a href=\"./index.php\">首页</a></p>";
+	echo "<p><a href=\"./wap.php\">首页</a></p>";
 	wap_footer();
 }
 //twitter list
@@ -138,7 +138,7 @@ if ($action == 'twitter')
 
 	$sql =" SELECT * FROM {$db_prefix}twitter ORDER BY id DESC  LIMIT $start_limit, $index_twnum";
 	$twnum = $sta_cache['twnum'];
-	$pageurl= './index.php?action=twitter&amp;page';
+	$pageurl= './wap.php?action=twitter&amp;page';
 	$query = $DB->query($sql);
 	while($row = $DB->fetch_array($query))
 	{
@@ -155,7 +155,7 @@ if ($action == 'twitter')
 	{
 		foreach ($tws as $val)
 		{
-			$doact = ISLOGIN===true?"<a href=\"./index.php?action=del_tw&amp;id=".$val['id']."\">删除</a>":'';
+			$doact = ISLOGIN===true?"<a href=\"./wap.php?action=del_tw&amp;id=".$val['id']."\">删除</a>":'';
 			echo $val['content'].$doact.'('.$val['date'].')<br />';
 		}
 	}else
@@ -163,7 +163,7 @@ if ($action == 'twitter')
 		echo 'No twitter yet!';
 	}
 	$stamp = time();
-	echo "</p><p>$page_url <br /><a href=\"./index.php?stamp=$stamp\">首页</a></p>";
+	echo "</p><p>$page_url <br /><a href=\"./wap.php?stamp=$stamp\">首页</a></p>";
 	wap_footer();
 }
 if ($action == 'waplogin')
@@ -172,13 +172,13 @@ if ($action == 'waplogin')
 	echo "<p>用户:<input name=\"user\" type=\"text\"  format=\"M*m\"/></p>\n";
 	echo "<p>密码:<input name=\"pw\" type=\"password\"  format=\"M*m\"/></p>\n";
 	echo "<p><anchor title=\"submit\">登录\n";
-	echo "<go href=\"index.php?action=login\" method=\"post\">\n";
+	echo "<go href=\"wap.php?action=login\" method=\"post\">\n";
 	echo "<postfield name=\"user\" value=\"$(user)\" />\n";
 	echo "<postfield name=\"pw\" value=\"$(pw)\" />\n";
 	echo "<postfield name=\"do\" value=\"dowaplogin\" />\n";
 	echo "</go></anchor>\n";
 	echo "</p>\n";
-	echo "<p><a href=\"index.php\">返回主页</a></p>\n";
+	echo "<p><a href=\"wap.php\">返回主页</a></p>\n";
 	wap_footer();
 }
 if ($action == 'addtw')
@@ -186,12 +186,12 @@ if ($action == 'addtw')
 	wap_header('Twitter');
 	echo "<p>内容:<br /><input name=\"tw\" type=\"text\"  format=\"M*m\"/></p>\n";
 	echo "<p><anchor title=\"submit\">提交\n";
-	echo "<go href=\"index.php?action=add_tw\" method=\"post\">\n";
+	echo "<go href=\"wap.php?action=add_tw\" method=\"post\">\n";
 	echo "<postfield name=\"tw\" value=\"$(tw)\" />\n";
 	echo "<postfield name=\"do\" value=\"dowaplogin\" />\n";
 	echo "</go></anchor>\n";
 	echo "</p>\n";
-	echo "<p><a href=\"index.php\">返回主页</a></p>\n";
+	echo "<p><a href=\"wap.php\">返回主页</a></p>\n";
 	wap_footer();
 }
 //新增 twitter
@@ -203,7 +203,7 @@ if(ISLOGIN === true && $action == 'add_tw')
 		$query = $DB->query("INSERT INTO {$db_prefix}twitter (content,date) VALUES('$content','$localdate')");
 		$MC->mc_twitter('../cache/twitter');
 		$MC->mc_sta('../cache/sta');
-		header("Location: index.php?action=twitter&amp;stamp=$time");
+		header("Location: wap.php?action=twitter&amp;stamp=$time");
 	}
 }
 //删除 twitter
@@ -213,7 +213,7 @@ if(ISLOGIN === true && $action == 'del_tw')
 	$query = $DB->query("DELETE FROM {$db_prefix}twitter WHERE id=$twid");
 	$MC->mc_twitter('../cache/twitter');
 	$MC->mc_sta('../cache/sta');
-	header("Location: index.php?action=twitter");
+	header("Location: wap.php?action=twitter");
 }
 //登陆验证
 if ($action == 'dowaplogin')
@@ -225,7 +225,7 @@ if ($action == 'dowaplogin')
 	$login_code == 'y'?$img_code = addslashes(trim(strtoupper($_POST['imgcode']))):$img_code = '';
 	if (strlen($username) >16)
 	{
-		header("Location: index.php");
+		header("Location: wap.php");
 	}
 	if (checkUser($username, $password,$img_code,$login_code))
 	{
@@ -236,10 +236,10 @@ if ($action == 'dowaplogin')
 		$_SESSION['adminname'] = $username;
 		$_SESSION['password'] = $password;
 		$stamp = time();
-		header("Location: index.php?stamp=$stamp");
+		header("Location: wap.php?stamp=$stamp");
 	}else
 	{
-		header("Location: index.php");
+		header("Location: wap.php");
 	}
 }
 
