fbec7a6652cc72c9832d2dc8688cf033b3e33bc0
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-11-04 02:49:01 +0000

    编写链接管理model类
    git-svn-id: http://emlog.googlecode.com/svn/trunk@702 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/link.php
+++ src/admin/link.php
@@ -7,89 +7,80 @@
  */
 
 require_once('./globals.php');
+require_once(EMLOG_ROOT.'/model/C_link.php');
+
+$emLink = new emLink($DB);
 
 if($action == '')
 {
+	$links = $emLink->getLinks();
 	include getViews('header');
-	$rs=$DB->query("SELECT * FROM ".DB_PREFIX."link ORDER BY taxis ASC");
-	$links = array();
-	while($rows=$DB->fetch_array($rs))
-	{
-		$rows['sitename'] = htmlspecialchars($rows['sitename']);
-		$rows['description'] = subString(htmlClean2($rows['description']),0,80);
-		$rows['siteurl'] = $rows['siteurl'];
-		$links[] = $rows;
-	}
 	require_once(getViews('links'));
-	include getViews('footer');cleanPage();
+	include getViews('footer');
+	cleanPage();
 }
-
-##################友情站点排序##################
+//排序
 if ($action== 'link_taxis')
 {
-	$link=$_POST['link'];
+	$link = isset($_POST['link']) ? $_POST['link'] : '';
 	if(!empty($link))
 	{
 		foreach($link as $key=>$value)
 		{
 			$value = intval($value);
-			$DB->query("UPDATE ".DB_PREFIX."link SET taxis='$value'  WHERE id='$key' ");
+			$emLink->updateLink(array('taxis'=>$value), $key);
 		}
 		$CACHE->mc_link('links');
 		formMsg('站点排序更新成功','./link.php',1);
 	}
 	formMsg('没有可排序项目','./link.php',0);
 }
-##################添加友情站点##################
+//添加
 if($action== 'addlink')
 {
-	if($_POST['sitename']=='' || $_POST['siteurl']=='')
+	$sitename = isset($_POST['sitename']) ? addslashes(trim($_POST['sitename'])) : '';
+	$siteurl = isset($_POST['siteurl']) ? addslashes(trim($_POST['siteurl'])) : '';
+	$description = isset($_POST['description']) ? addslashes(trim($_POST['description'])) : '';
+
+	if($sitename =='' || $siteurl =='')
 	{
 		formMsg('站点名称和地址不能为空','javascript:history.go(-1);',0);
 	}
-	$stitename=addslashes(trim($_POST['sitename']));
-	$siteurl=addslashes(trim($_POST['siteurl']));
-	$description=addslashes(trim($_POST['description']));
-	$sql=" insert into ".DB_PREFIX."link (sitename,siteurl,description) values(' ".$stitename." ',' ".$siteurl." ',' ".$description." ')";
-	$DB->query($sql);
+	$emLink->addLink($sitename, $siteurl, $description);
 	$CACHE->mc_link('links');
 	formMsg( '友情站点添加成功','./link.php',1);
 }
-####################友情站点修改编辑####################
-
+//修改
 if ($action== 'mod_link')
 {
-	include getViews('header');
-	
-	$linkid = isset($_GET['linkid']) ? intval($_GET['linkid']) : '';
-	
-	$sql = "select * from ".DB_PREFIX."link where id=$linkid "; 
-	$result =$DB->query($sql);
-	$show_link=$DB->fetch_array($result);
-	$sitename=htmlspecialchars(trim($show_link['sitename']));
-	$siteurl=htmlspecialchars(trim($show_link['siteurl']));
-	$description=htmlspecialchars(trim($show_link['description']));
+	$linkId = isset($_GET['linkid']) ? intval($_GET['linkid']) : '';
 
+	$linkData = $emLink->getOneLink($linkId);
+	extract($linkData);
+
+	include getViews('header');
 	require_once(getViews('linkedit'));
 	include getViews('footer');cleanPage();
 }
-
 if($action=='update_link')
 {
-	$sql=" UPDATE ".DB_PREFIX."link SET 
-	sitename='".addslashes($_POST['sitename'])."',
-	siteurl='".addslashes($_POST['siteurl'])."',
-	description='".addslashes($_POST['description'])."' where id='".intval($_POST['linkid'])."' ";
-	$DB->query($sql);
+	$sitename = isset($_POST['sitename']) ? addslashes(trim($_POST['sitename'])) : '';
+	$siteurl = isset($_POST['siteurl']) ? addslashes(trim($_POST['siteurl'])) : '';
+	$description = isset($_POST['description']) ? addslashes(trim($_POST['description'])) : '';
+	$linkId = isset($_POST['linkid']) ? intval($_POST['linkid']) : '';
+
+	$emLink->updateLink(array('sitename'=>$sitename, 'siteurl'=>$siteurl, 'description'=>$description), $linkId);
+
 	$CACHE->mc_link('links');
 	formMsg("修改成功","./link.php",1);
 }
-//删除友情连接
+//删除
 if ($action== 'dellink')
 {
 	$linkid = isset($_GET['linkid']) ? intval($_GET['linkid']) : '';
-	$DB->query("DELETE FROM ".DB_PREFIX."link where id=$linkid");
+	$emLink->deleteLink($linkid);
 	$CACHE->mc_link('links');
 	formMsg('删除成功','./link.php',1);
 }
+
 ?>
---
+++ src/model/C_link.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * 模型：友情链接
+ * @copyright (c) 2008, Emlog All Rights Reserved
+ * @version emlog-2.7.0
+ * $Id: comment.php 682 2008-10-14 16:08:01Z emloog $
+ */
+
+
+class emLink {
+
+	var $dbhd;
+	var $linkTable;
+
+	function emLink($dbhandle)
+	{
+		$this->dbhd = $dbhandle;
+		$this->linkTable = DB_PREFIX.'link';
+	}
+
+	function getLinks()
+	{
+		$res = $this->dbhd->query("SELECT * FROM $this->linkTable ORDER BY taxis ASC");
+		$links = array();
+		while($row = $DB->fetch_array($res))
+		{
+			$row['sitename'] = htmlspecialchars($row['sitename']);
+			$row['description'] = subString(htmlClean2($row['description']),0,80);
+			$row['siteurl'] = $row['siteurl'];
+			$links[] = $row;
+		}
+		return $links;
+	}
+
+	function updateLink($linkData, $linkId)
+	{
+		$Item = array();
+		foreach ($linkData as $key => $data)
+		{
+			$Item[] = "$key='$data'";
+		}
+		$upStr = implode(',', $Item);
+		$this->dbhd->query("update $this->linkTable set $upStr where gid=$linkId");
+	}
+
+	function addLink($name, $url, $des)
+	{
+		$sql="insert into $this->linkTable (sitename,siteurl,description) values('$name','$url.','$des')";
+		$DB->query($sql);
+	}
+
+	function getOneLink($linkId)
+	{
+		$sql = "select * from $this->linkTable where id=$linkId ";
+		$res = $this->dbhd->query($sql);
+		$row = $this->dbhd->fetch_array($res);
+		$linkData = array();
+		if($row)
+		{
+			$linkData = array(
+			'sitename' => htmlspecialchars(trim($row['sitename'])),
+			'siteurl' => htmlspecialchars(trim($row['siteurl'])),
+			'description' => htmlspecialchars(trim($row['description']))
+			);
+		}
+		return $linkData;
+	}
+	
+	function deleteLink($linkId)
+	{
+		$DB->query("DELETE FROM $this->linkTable where id=$linkId");
+	}
+
+}
+
+?>
