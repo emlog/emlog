14ddceda5d0bdbd6e5f47ff2dda80b0e344f054a
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-11-20 07:03:24 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@727 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/lib/C_checkcode.php
+++ src/lib/C_checkcode.php
@@ -9,14 +9,11 @@
 session_start();
 header("Content-Type:image/png");
 
-//创建图像标识符
 $img = ImageCreate(45,22);
 
-//颜色
 $white = ImageColorAllocate($img,153,153,204);
 $bgcolor = ImageColorAllocate($img,255,255,255);
 
-//随机验证码
 $letter = array('A','b','c','d','E','f','g','h','i','j','K','L','m','n','o','p','q','r','s','T','u','v','W','x','y','z','3','4','5','8');
 for ($i = 1;$i <= 4;$i++) {
 	$randcode .= $letter[mt_rand(0,29)];
@@ -25,7 +22,7 @@ for ($i = 1;$i <= 4;$i++) {
 $_SESSION['code'] = strtoupper($randcode);
 
 //绘制图像
-imagefill($img,0,0,$bgcolor);//背景色填充
+imagefill($img,0,0,$bgcolor);
 ImageString($img,5,6,3,$randcode,$white);
 for ($j=0;$j<80;$j++) {
 	$x = mt_rand(0,55);
@@ -33,9 +30,8 @@ for ($j=0;$j<80;$j++) {
 	ImageSetPixel($img,$x,$y,$white);
 }
 
-//输出
 Imagepng($img);
 
-//结束
 ImageDestroy($img);
+
 ?>
