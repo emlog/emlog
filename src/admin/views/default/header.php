d67460c82773d009e57de03e3cafda443699778f
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-12-21 02:56:40 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@791 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/admin/views/default/header.php
+++ src/admin/views/default/header.php
@@ -21,7 +21,7 @@
     <td class="vesion"><?php echo EMLOG_VERSION; ?></td>
     <td class="headtext"><a href="../index.php" target="_blank" title="在新窗口浏览我的blog"><?php echo $blogname; ?></a></td>
     <td align=right nowrap class="headtext">
-	欢迎您：<a href="blogger.php" title="点击修改个人资料"><?php if($userData['nickname']):echo $userData['nickname'];else:echo $userData['username'];endif;?></a>&nbsp;&nbsp;|&nbsp;&nbsp;
+	你好：<a href="blogger.php" title="点击修改个人资料"><?php if($userData['nickname']):echo $userData['nickname'];else:echo $userData['username'];endif;?></a>&nbsp;&nbsp;|&nbsp;&nbsp;
 	<a href="configure.php">博客设置</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 	<a href="./index.php">管理首页</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 	<a href="index.php?action=logout">退出</a>&nbsp;&nbsp;&nbsp;&nbsp;	</td>
