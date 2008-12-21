0b6d29383f783e31e58859c51ccb1fbbd246827b
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2008-12-21 03:09:28 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@792 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/admin/views/default/index.php
+++ src/admin/views/default/index.php
@@ -34,10 +34,6 @@ $allcom = $hidecom + $comnum;
 <table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
 	<tr>
 		<td width="50%">官方主页: <a href="http://www.emlog.net" target="_blank">www.emlog.net</a></font></td>
-		<td width="50%">官方博客: <a href="http://www.emlog.net/blog" target="_blank">every memory log</a></td>
-	</tr>
-	<tr>
-		<td width="50%">工程控制: <a href="http://code.google.com/p/emlog/" target="_blank">google-code.emlog</a></td>
 		<td width="50%">电子邮件: <a href="mailto:emloog@gmail.com">emloog@gmail.com</a></td>
 	</tr>
 </table>
