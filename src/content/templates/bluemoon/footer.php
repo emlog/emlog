fe7b0191001b1156d22d09568a6c6ea7fb3a6611
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-01-07 03:32:09 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@838 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/content/templates/bluemoon/footer.php
+++ src/content/templates/bluemoon/footer.php
@@ -1,7 +1,7 @@
 <?php if(!defined('EMLOG_ROOT')) {exit('error!');} ?>
 <div id="footer">
 	<p><a href="./rss.php" target="_blank">Entries (RSS)</a><br/>
-    <font color="#B26B58">powered by <a class="sec" href="http://www.emlog.net" target="_blank">emlog</a> and <a class="sec" href="http://luckythemes.com" target="_blank">Lucky Themes</a></font></p><br />
+    <font color="#B26B58">powered by <a class="sec" href="http://www.emlog.net" target="_blank" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a> and <a class="sec" href="http://luckythemes.com" target="_blank">Lucky Themes</a></font></p><br />
 	<a class="sec" href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a>
 </div>
 <br />
