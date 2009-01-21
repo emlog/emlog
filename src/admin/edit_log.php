86b19f72f76fe0409f619bc6053775aa538dcd0b
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-01-21 15:30:31 +0000

    修改编辑日志后返回的链接
    git-svn-id: http://emlog.googlecode.com/svn/trunk@863 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/edit_log.php
+++ src/admin/edit_log.php
@@ -103,5 +103,5 @@ if($action == 'edit')
 	$CACHE->mc_record();
 	$CACHE->mc_tags();
 	$CACHE->mc_newlog();
-	formMsg( "保存成功\t$tbmsg","javascript:history.go(-1);",1);
+	formMsg( "保存成功\t$tbmsg", "./edit_log.php?gid=$logid", 1);
 }
