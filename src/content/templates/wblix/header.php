5e8599aaef0d4f1237940afcc3ec47ab7036e216
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-02-19 02:03:27 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@890 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/content/templates/wblix/header.php
+++ src/content/templates/wblix/header.php
@@ -34,7 +34,9 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 <?php if(ISLOGIN): ?>
 	<li><a href="./admin/write_log.php">写日志</a></li>
 	<li><a href="./admin/">管理中心</a></li>
-	<li><a href="./index.php?action=logout">退出</a></li>
+	<li><a href="./admin/index.php?action=logout">退出</a></li>
+<?php else: ?>
+	<li><a href="./admin/index.php">登录</a></li>
 <?php endif; ?>
 </ul>
 
