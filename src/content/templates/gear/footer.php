9f8af8b55fd4066ec0642bb7beaac2fc39bd426b
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-02-21 16:01:02 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@896 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/content/templates/gear/footer.php
+++ src/content/templates/gear/footer.php
@@ -7,8 +7,7 @@
 
 						<div class="footer">
 				<div class="paddings">
-					<div class="fl">powered by <a href="http://www.emlog.net" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a><br />
-<a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a></div>
+					<div class="fl">Powered by <a href="http://www.emlog.net" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a> <a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a></div>
 					<div class="fr">Designed by <a href="http://www.mymobiles.com">My Mobiles</a></div>
 				</div>
 				<div class="clear"></div>
