639b7c092bcfd233715cba12e0121a774969766d
jflyang <jflyang@5f219224-4642-0410-9343-2d18db063b08>
2009-02-22 06:10:46 +0000

    新模板clodbule
    git-svn-id: http://emlog.googlecode.com/svn/trunk@903 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php if(!defined('EMLOG_ROOT')) {exit('error!');} ?>
+				</ul>
+
+</div>
+
+<div id="footer">
+	<p id="blog-name">powered by 
+<a href="http://www.emlog.net" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a><br />
+<a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a></p>
+	<p id="webrevolutionary-link">
+				<a href="http://webrevolutionary.com/coldblue/">ColdBlue</a> v1.0 &mdash; A theme by <a href="http://webrevolutionary.com/">WebRevolutionary</a> &amp; <a href="http://www.forwebdesigners.com/">ForWebdesigners</a>
+	</p>
+</div>
+
+<!-- ColdBlue v1.0 theme designed by WebRevolutionary.com -->
+
+		</body>
+</html>
