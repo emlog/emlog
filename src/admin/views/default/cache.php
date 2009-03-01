5bae9383b73530cff163cd21b72c9852f29dfe1c
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-03-01 03:20:18 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@939 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/admin/views/default/cache.php
+++ src/admin/views/default/cache.php
@@ -6,11 +6,11 @@
 <div class=line></div>
 <table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
    <tr>
-   <td align="center" class="notice">您现在不必每次对后台进行修改后（包括：写日志、修改参数、评论删除、等等一切后台的改动）都来更新缓存，<br />
-              但是考虑到一些特殊情况导致缓存文件被无意修改给你带来不便，<br />
-              我们仍旧提供更新全部缓存文件的功能！</td>
+   <td align="center" class="notice">
+   缓存技术可以大幅度加快你博客首页的加载速度。<br>通常系统会自动更新缓存，但也有些特殊情况需要你手动更新，<br>比如缓存文件被无意修改、你手动修改过数据库 等。
+   </td>
   </tr>
-<tr><td align="center"><a href="cache.php?action=mkcache">重建所有缓存</a></td></tr>  
+<tr><td align="center"><a href="cache.php?action=mkcache">重建缓存</a></td></tr>  
 </table><p>
 </td>
 </tr>
