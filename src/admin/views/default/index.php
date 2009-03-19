c3278a045480c462760031545d8abb370887abc0
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-03-19 00:32:24 +0000

    开始修改后台首页
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1015 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/default/index.php
+++ src/admin/views/default/index.php
@@ -2,44 +2,30 @@
 if(!defined('ADMIN_ROOT')) {exit('error!');}
 $allcom = $hidecom + $comnum;
 ?>
-<div class=containertitle></div>
-<table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
-	<tr>
-		<td width="50%">
-		日志: <span class=care2><b><?php echo $lognum; ?></b></span> | 
-		引用: <span class=care2><b><?php echo $tbnum; ?></b></span>  | 
-		评论: <span class=care2><b><?php echo $allcom; ?></b></span> 
-		<?php if($ischkcomment == 'y' || $hidecom != 0): ?>
-		| 待审核评论: <b><a href="./comment.php?hide=y"><?php echo $hidecom; ?></a></b>
-		<?php endif; ?>
-		</td>
-	</tr>
-</table>
-<div class=line2></div>
-<table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
-	<tr>
-		<td width="50%">服务器环境: <?php echo $serverapp; ?></td>
-		<td width="50%">PHP版本: <?php echo $php_ver; ?></td>
-	</tr>
-	 <tr>
-		<td width="50%">MySQL数据库: <?php echo $mysql_ver; ?></td>
-		<td width="50%">服务器时间: <?php echo $serverdate; ?></td>
-	</tr>
-	<tr>
-		<td width="50%">GD图形处理库: <?php echo $gd_ver; ?></td>
-		<td><a href="configure.php?action=phpinfo">[phpinfo]</a></td>
-	</tr>
-</table>
-<div class=line2></div>
-<table width="95%" align="center" border="0" cellspacing="1" cellpadding="4" class="formtd2">
-	<tr>
-		<td width="50%">官方主页: <a href="http://www.emlog.net" target="_blank">www.emlog.net</a></font></td>
-		<td width="50%">电子邮件: <a href="mailto:emloog@gmail.com">emloog@gmail.com</a></td>
-	</tr>
-</table>
-</td>
-</tr>
-</table>
-</td>
-</tr>
-</table>
+<div id="admindex">
+<div id="admindex_main">
+我有<span class=care2><b><?php echo $lognum; ?></b></span>篇日志，<span class=care2><b><?php echo $tbnum; ?></b></span>条引用通告，<span class=care2><b><?php echo $allcom; ?></b></span>条评论<?php if($ischkcomment == 'y' || $hidecom != 0): ?>，其中有<b><a href="./comment.php?hide=y"><?php echo $hidecom; ?></a></b>条评论等待审核<?php endif; ?>。
+</div>
+<div id="admindex_servinfo">
+<h3>服务器信息</h3>
+<ul>
+	<li>服务器环境: <?php echo $serverapp; ?></li>
+	<li>PHP版本: <?php echo $php_ver; ?></li>
+	<li>MySQL数据库: <?php echo $mysql_ver; ?></li>
+	<li>服务器时间: <?php echo $serverdate; ?></li>
+	<li>GD图形处理库: <?php echo $gd_ver; ?></li>
+	<li><a href="configure.php?action=phpinfo">[phpinfo]</a></li>
+</ul>
+</div>
+<div id="admindex_msg">
+<h3>官方信息</h3>
+<ul>
+	<li>服务器环境: <?php echo $serverapp; ?></li>
+	<li>PHP版本: <?php echo $php_ver; ?></li>
+	<li>MySQL数据库: <?php echo $mysql_ver; ?></li>
+	<li>服务器时间: <?php echo $serverdate; ?></li>
+	<li>GD图形处理库: <?php echo $gd_ver; ?></li>
+	<li><a href="configure.php?action=phpinfo">[phpinfo]</a></li>
+</ul>
+</div>
+</div>
