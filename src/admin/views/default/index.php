7ccee785f8d74012bd2860cd2e51a71c68b66bab
jflyang@gmail.com <jflyang@gmail.com@5f219224-4642-0410-9343-2d18db063b08>
2009-03-20 08:20:53 +0000

    后台首页布局
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1017 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/default/index.php
+++ src/admin/views/default/index.php
@@ -6,16 +6,17 @@ $allcom = $hidecom + $comnum;
 <div id="admindex_main">
 我有<span class=care2><b><?php echo $lognum; ?></b></span>篇日志，<span class=care2><b><?php echo $tbnum; ?></b></span>条引用通告，<span class=care2><b><?php echo $allcom; ?></b></span>条评论<?php if($ischkcomment == 'y' || $hidecom != 0): ?>，其中有<b><a href="./comment.php?hide=y"><?php echo $hidecom; ?></a></b>条评论等待审核<?php endif; ?>。
 </div>
+<div class="clear"></div>
 <div id="admindex_servinfo">
 <h3>服务器信息</h3>
-<ul>
+<ol>
 	<li>服务器环境: <?php echo $serverapp; ?></li>
 	<li>PHP版本: <?php echo $php_ver; ?></li>
 	<li>MySQL数据库: <?php echo $mysql_ver; ?></li>
 	<li>服务器时间: <?php echo $serverdate; ?></li>
 	<li>GD图形处理库: <?php echo $gd_ver; ?></li>
-	<li><a href="configure.php?action=phpinfo">[phpinfo]</a></li>
-</ul>
+	<div><a href="configure.php?action=phpinfo">[phpinfo]</a></div>
+</ol>
 </div>
 <div id="admindex_msg">
 <h3>官方信息</h3>
@@ -28,4 +29,5 @@ $allcom = $hidecom + $comnum;
 	<li><a href="configure.php?action=phpinfo">[phpinfo]</a></li>
 </ul>
 </div>
+<div class="clear"></div>
 </div>
