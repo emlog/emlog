faf9c92e19af2b631227ae4758fbd11215edfb58
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-04-06 10:30:02 +0000

    修改html标签属性
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1035 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/content/templates/devart/header.php
+++ src/content/templates/devart/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="keywords" content="<?php echo $site_key; ?>" />
