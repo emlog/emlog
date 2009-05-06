4adda10e430d468c1b731e8bafcd53510966e966
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-05-06 10:09:56 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@1121 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/admin/views/default/index.php
+++ src/admin/views/default/index.php
@@ -1,15 +1,15 @@
 <?php if(!defined('ADMIN_ROOT')) {exit('error!');}?>
 <div id="admindex">
 <div id="admindex_main">
-目前有<span class=care2><b><?php echo ROLE == 'admin' ? $sta_cache['lognum'] : $user_cache[UID]['lognum']; ?></b></span>篇日志，
+目前有<span class=care2><b><?php echo ROLE == 'admin' ? $sta_cache['lognum'] : $user_cache[UID]['lognum']; ?></b></span>篇日志 ，
 <span class=care2><b><?php echo ROLE == 'admin' ? $sta_cache['comnum_all'] : $user_cache[UID]['commentnum']; ?></b></span>条评论
-<?php 
+<?php
 $hidecmnum = ROLE == 'admin' ? $sta_cache['hidecomnum'] : $user_cache[UID]['hidecommentnum'];
 if ($hidecmnum > 0):
 ?>
 (未审核:<b><a href="./comment.php?hide=y"><?php echo $hidecmnum; ?></a></b>)
 <?php endif; ?>
-<span class=care2>，<b><?php echo ROLE == 'admin' ? $sta_cache['tbnum'] : $user_cache[UID]['tbnum']; ?></b></span>条引用通告
+，<span class=care2><b><?php echo ROLE == 'admin' ? $sta_cache['tbnum'] : $user_cache[UID]['tbnum']; ?></b></span>条引用通告
 </div>
 <div class="clear"></div>
 <div id="admindex_servinfo">
