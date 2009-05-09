0b3d10dd06974f29e1d5b1d4ac5ecafbfbca5e3e
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-05-09 11:05:46 +0000

    解决多个文件附件 后台管理无法区分的问题。鼠标悬浮提示文件名
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1148 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/default/attlib.php
+++ src/admin/views/default/attlib.php
@@ -44,7 +44,7 @@ foreach($attach as $key=>$value):
 		$embedlink = '';
 	}
 ?>
-	<li id="attlist"><a href="<?php echo $atturl; ?>" target="_blank"><img src="<?php echo $imgpath; ?>" width="60" height="60" border="0" align="absmiddle"/></a>
+	<li id="attlist"><a href="<?php echo $atturl; ?>" target="_blank" title="<?php echo $value['filename']; ?>"><img src="<?php echo $imgpath; ?>" width="60" height="60" border="0" align="absmiddle"/></a>
 	<br><a href="javascript: em_confirm(<?php echo $value['aid']; ?>, 'attachment');">删除</a> <?php echo $embedlink; ?></li>
 <?php endforeach; endif; ?>
 </div>
