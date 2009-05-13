8a2284acab6762e4041e9420be3712c29b2d90c7
jflyang@gmail.com <jflyang@gmail.com@5f219224-4642-0410-9343-2d18db063b08>
2009-05-13 10:14:34 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@1186 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/content/templates/default/log_list.php
+++ src/content/templates/default/log_list.php
@@ -11,14 +11,14 @@ $topFlg = $value['toplog'] == 'y' ? "<img src=\"{$em_tpldir}images/import.gif\" 
 	</h2>
 	<?php blog_sort($value['sortid'], $value['logid']); ?>
 	<div class="clear line"></div>
-   	<div class="date"> post by <?php echo blog_author($value['author']); ?> / <?php echo date('Y-n-j G:i l', $value['date']); ?></div>
+   	<div class="bloger"> post by <?php echo blog_author($value['author']); ?> / <?php echo date('Y-n-j G:i l', $value['date']); ?></div>
 	<div class="post"><?php echo $value['log_description']; ?></div>
 	<div class="fujian"><?php blog_att($value['logid']); ?></div>
 	<div class="under">
 	<div class="top"></div>
 	<div class="under_p">
 	<div class="tag"><?php blog_tag($value['logid']); ?></div>
-	<div class="date"></div>
+	<div class="editor"><a href="#">编辑</a></div>
 	<div>
 	<a href="./?post=<?php echo $value['logid']; ?>#comment">评论(<?php echo $value['comnum']; ?>)</a>
 	<a href="./?post=<?php echo $value['logid']; ?>#tb">引用(<?php echo $value['tbcount']; ?>)</a> 
