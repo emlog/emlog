da6661b492b9260677249d03d04fc792a4737b15
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-05-18 23:31:10 +0000

    删除black模板
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1212 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/content/templates/black/footer.php
+++
@@ -1,7 +0,0 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');} ?>
-</div>
-</div>
-<div id="siteCopyright">Powered by 
-<a href="http://www.emlog.net" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a>  Theme Modified by <a href="http://www.emlog.net/qiyu/" target="_blank">奇遇</a>
-<a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a>
-</div>
