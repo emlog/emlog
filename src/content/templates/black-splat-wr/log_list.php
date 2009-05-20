2b0531d292ab3336ec2aebc9dc98b5876532d2b7
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-05-20 12:07:21 +0000

    git-svn-id: http://emlog.googlecode.com/svn/trunk@1226 5f219224-4642-0410-9343-2d18db063b08 --HG-- branch : _bitbucket

--- src/content/templates/black-splat-wr/log_list.php
+++ src/content/templates/black-splat-wr/log_list.php
@@ -3,7 +3,7 @@
 <div class="post">
 <h2 class="postTitle"><?php topflg($value['top']); ?><a href="<?php echo BLOG_URL; ?>?post=<?php echo $value['logid']; ?>"><?php echo $value['log_title']; ?></a></h2>
 <p class="postMeta">filed in <?php blog_sort($value['sortid'], $value['logid']); ?>
-<span class="sort">post by <?php blog_author($value['author']); ?> / <?php echo date('Y-n-j G:i l', $value['date']); ?></span> <?php editflg($logid,$author); ?></p>
+<span class="sort">post by <?php blog_author($value['author']); ?> / <?php echo date('Y-n-j G:i l', $value['date']); ?></span> <?php editflg($value['logid'],$author); ?></p>
 <div class="postContent"><?php echo $value['log_description']; ?></div>
 <p class="tags"><?php blog_att($value['logid']); ?></p>
 <p class="tags"><?php blog_tag($value['logid']); ?></p>
