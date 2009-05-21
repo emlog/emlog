14c6b3d7120117cd461e2bed507f702c43cf1929
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-05-21 02:29:01 +0000

    优化相册插件
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1227 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/content/plugins/picasa_album/album.php
+++ src/content/plugins/picasa_album/album.php
@@ -1,14 +1,13 @@
 <?php
 /**
  * emlog相册插件
- * by：dawei
+ * by：emlog
  */
 require_once('XMLParser.php');
 require_once('../../../common.php');
 
 $cerTemplatePath = TEMPLATE_PATH.$nonce_templet.'/';
 $calendar_url = BLOG_URL.'calendar.php?' ;
-
 
 $album = isset($_GET['album']) ? $_GET['album'] : '';
 
@@ -19,17 +18,14 @@ if(@$fp = fopen($cachefile, 'r'))
 	$account = @fread($fp,filesize($cachefile));
 	fclose($fp);
 }
-
+//显示相册列表
 if (!$album)
 {
 	$XMLP = new SofeeXmlParser();
-
 	$feed = "http://picasaweb.google.com/data/feed/base/user/{$account}?kind=album&access=public&hl=zh_CN";
-
 	$XMLP->parseFile($feed);
 
 	$albumData = $XMLP->getTree();
-
 	$albumData = $albumData['feed'];
 
 	$blogtitle =  $albumData['author']['name']['value'] . '的相册 - ' . $blogname;
@@ -48,14 +44,14 @@ if (!$album)
 		$thumb_width = $thumb['width'];
 		$thumb_height = $thumb['height'];
 		$log_content .=	'
-	<div>
-	<div>
-	<a href="album.php?album='.$albumId.'">
-	<img src="'.$thumb_url.'" width="'.$thumb_width.'" height="'.$thumb_height.'"></a>
-	</div>
-	<p><a href="album.php?album='.$albumId.'">'.$title.'</a></p>
-	<p>'.$description.'</p>
-	</div>';
+		<div>
+		<div>
+		<a href="album.php?album='.$albumId.'">
+		<img src="'.$thumb_url.'" width="'.$thumb_width.'" height="'.$thumb_height.'"></a>
+		</div>
+		<p><a href="album.php?album='.$albumId.'">'.$title.'</a></p>
+		<p>'.$description.'</p>
+		</div>';
 	}
 
 	$allow_remark = 'n';
@@ -64,22 +60,21 @@ if (!$album)
 	include getViews('header');
 	include getViews('page');
 }
-
+//显示单个相册里的照片
 if ($album)
 {
 	$XMLP = new SofeeXmlParser();
 	$feed = "http://picasaweb.google.com/data/feed/base/user/{$account}/albumid/".$album."?hl=zh_CN";
-
 	$XMLP->parseFile($feed);
-	$albumData = $XMLP->getTree();
 
+	$albumData = $XMLP->getTree();
 	$albumData = $albumData['feed'];
 
 	$blogtitle =  $albumData['title']['value'] . ' - ' . $blogname;
 	$log_title =  $albumData['title']['value'];
-	$log_content = '<div id="gallery">
-    <ul>';
-
+	$log_content = '
+	<div id="gallery">
+	<ul>';
 	foreach ($albumData['entry'] as $val)
 	{
 		$thumb = $val['media:group']['media:thumbnail'][1];
@@ -89,13 +84,13 @@ if ($album)
 		$photo_src = preg_replace('/^(.+\/)(s\d+)(.+)$/', '$1s512$3', $thumb_url);
 
 		$log_content .=	'
-
         <li>
 		<a href="'.$photo_src.'">
 		<img src="'.$thumb_url.'" width="'.$thumb_width.'" height="'.$thumb_height.'"></a>
 		</li>';
 	}
-	$log_content .= '</ul>
+	$log_content .= '
+	</ul>
     </div>';
 
 	$allow_remark = 'n';
@@ -107,16 +102,16 @@ if ($album)
 	include getViews('page');
 }
 
-
+//相册css样式
 function album_css()
 {
-	echo <<<EOT
+echo <<<EOT
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/jquery.lightbox-0.5.js"></script>
 <link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" media="screen" />
 <script type="text/javascript">
 $(function() {
-	$('#gallery a').lightBox();
+$('#gallery a').lightBox();
 });
 </script>
 <style type="text/css">
