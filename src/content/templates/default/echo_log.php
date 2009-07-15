c8e4fcb54e5d4984f8e206f2532ceca3239bb469
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-07-15 00:05:56 +0000

    回退对默认模板的误操作
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1369 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

---
+++ src/content/templates/default/echo_log.php
@@ -0,0 +1,25 @@
+<?php if(!defined('EMLOG_ROOT')) {exit('error!');} ?>
+<div id="content">
+<ul>
+<li>
+	<h2 class="content_h2"><?php topflg($top); ?><?php echo $log_title; ?></h2>
+	<div class="act"><?php blog_sort($sortid, $logid); ?></div>
+	<div class="editor"><?php editflg($logid,$author); ?></div>
+	<div class="clear line"></div>
+    <div class="bloger">post by <?php blog_author($author); ?> / <?php echo date('Y-n-j G:i l', $date); ?></div>
+	<div class="post"><?php echo $log_content; ?></div>
+	<div class="fujian"><?php blog_att($logid); ?></div>
+	<div class="tag echo_tag"><?php blog_tag($logid); ?></div>
+	<?php doAction('log_related'); ?>
+	<div class="nextlog"><?php neighbor_log(); ?></div>
+	<?php blog_trackback(); ?>
+	<?php blog_comments(); ?>
+	<?php if ($allow_remark == 'y'){blog_comments_post();}?>
+</li>
+</ul>
+</div>
+<!--end content-->
+<?php 
+include getViews('side');
+include getViews('footer'); 
+?>
