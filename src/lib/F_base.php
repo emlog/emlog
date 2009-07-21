e4a01eb647982bbcb4b80b73419489ec23056c51
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-07-21 16:30:33 +0000

    修复朦胧发现的两处安全隐患
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1375 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/lib/F_base.php
+++ src/lib/F_base.php
@@ -71,7 +71,7 @@ function htmlClean($content, $wrap=true)
 }
 
 /**
- * 获取用户ip
+ * 获取用户ip地址
  *
  * @return string
  */
@@ -80,22 +80,26 @@ function getIp()
 	if (isset($_SERVER))
 	{
 		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-			$realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-			$realip = $_SERVER['HTTP_CLIENT_IP'];
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
 		} else {
-			$realip = $_SERVER['REMOTE_ADDR'];
+			$ip = $_SERVER['REMOTE_ADDR'];
 		}
 	} else {
 		if (getenv("HTTP_X_FORWARDED_FOR")) {
-			$realip = getenv( "HTTP_X_FORWARDED_FOR");
+			$ip = getenv( "HTTP_X_FORWARDED_FOR");
 		} elseif (getenv("HTTP_CLIENT_IP")) {
-			$realip = getenv("HTTP_CLIENT_IP");
+			$ip = getenv("HTTP_CLIENT_IP");
 		} else {
-			$realip = getenv("REMOTE_ADDR");
+			$ip = getenv("REMOTE_ADDR");
 		}
 	}
-	return $realip;
+	if($ip && !preg_match("/\d+\.\d+\.\d+\.\d+/", $ip))
+	{
+		$ip = '';
+	}
+	return $ip;
 }
 
 /**
