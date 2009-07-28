e7fa00da68cef69080045f68f64889b285b4967f
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-07-28 15:50:25 +0000

    1、移除默认模板以外的其他模板到 emlog扩展库项目
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1385 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/content/templates/g7/footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
-<div id="footer"><div class="ftr">
-Powered by 
-<a href="http://www.emlog.net" title="emlog <?php echo EMLOG_VERSION;?>">emlog</a> <a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a>
-<?php doAction('index_footer'); ?> Theme produced by <a href="http://www.underone.com/">underone</a></p>
- </div>  
-</div>
-</body>
-</html> 
