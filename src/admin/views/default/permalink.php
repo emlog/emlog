204ecafcc3102f61bfeb4a1766d0f741c083fdde
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-11-07 07:53:17 +0000

    优化固定连接设置
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1504 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/default/permalink.php
+++ src/admin/views/default/permalink.php
@@ -7,17 +7,18 @@ setTimeout(hideActived,2600);
 <?php if(isset($_GET['error'])):?><span class="error">保存失败：配置文件不可写</span><?php endif;?>
 <div class=containertitle><a href="./configure.php">基本设置</a> <a href="./permalink.php">固定连接</a></div>
 <div class=line></div>
+<div class="des">你可以在这里修改日志链接的形式，以此提高链接的可读性和对搜索引擎的友好程度。<br />如果修改后日志无法访问，请修改回默认形式。</div>
 <form action="permalink.php?action=update" method="post">
-  <table cellspacing="8" cellpadding="4" width="95%" align="center" border="0">
+  <table cellspacing="8" cellpadding="4" width="95%" border="0">
     <tbody>
       <tr>
-        <td><input type="radio" name="permalink" value="0" <?php echo $ex0; ?>> <?php echo BLOG_URL; ?>?post=1</td>
+        <td><input type="radio" name="permalink" value="0" <?php echo $ex0; ?>>默认形式：<span class="permalink_url"><?php echo BLOG_URL; ?>?post=1</span></td>
       </tr>
 	  <tr>
-        <td><input type="radio" name="permalink" value="1" <?php echo $ex1; ?>> <?php echo BLOG_URL; ?>post-1.html</td>
+        <td><input type="radio" name="permalink" value="1" <?php echo $ex1; ?>>文件形式：<span class="permalink_url"><?php echo BLOG_URL; ?>post-1.html</span></td>
       </tr>
 	  <tr>
-        <td><input type="radio" name="permalink" value="2" <?php echo $ex2; ?>> <?php echo BLOG_URL; ?>post/1</td>
+        <td><input type="radio" name="permalink" value="2" <?php echo $ex2; ?>>目录形式：<span class="permalink_url"><?php echo BLOG_URL; ?>post/1</span></td>
       </tr>
       <tr>
         <td rowspan="2"><input type="submit" value="保存设置" class="button" /></td>
