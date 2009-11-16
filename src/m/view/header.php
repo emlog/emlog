52e9010a6c1be38e5d079893c80a239951239366
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2009-11-16 13:34:08 +0000

    手机版本优化
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1516 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/m/view/header.php
+++ src/m/view/header.php
@@ -7,33 +7,30 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <head>
 <title><?php echo $blogname; ?></title>
 <style type="text/css" id="internalStyle">
-body{ background-color:#FFFFFF; font-size:14px; margin: 0; padding: 0;font-family: Helvetica, Arial, sans-serif;-webkit-text-size-adjust: none;}
-a:link,a:visited,a:hover,a:active {text-decoration: none;color: #333;}
+body{background-color:#FFFFFF; font-size:14px; margin: 0; padding:0;font-family: Helvetica, Arial, sans-serif;-webkit-text-size-adjust: none;}
+a:link,a:visited,a:hover,a:active {text-decoration: none;color:#333;}
 #top{background-color:#32598B; padding:8px 8px;}
-#footer{background-color:#EFEFEF; color:#666666; padding:5px; text-align:center; font-weight:bold;}
+#footer{background-color:#EFEFEF; color:#666666; padding:5px;text-align:center;font-weight:bold;}
 #page{text-align:center;font-size:26px; color: #CCCCCC}
-#page a:link,a:active,a:visited,a:hover{padding:0px 6px;}
-#m{ padding:10px;}
-#blogname{ font-weight:bold; color:#FFFFFF; font-size:14px;}
-#blogdes{ color:#FFFFFF; margin:5px 0px 0px 0px;}
+#page a:link,a:active,a:visited,a:hover{padding:0px 6px;}#m{padding:10px;}
+#blogname{font-weight:bold; color:#FFFFFF; font-size:14px;}
+#blogdes{color:#FFFFFF; margin:5px 0px 0px 0px;}
 #navi{background:#B2D281; padding:3px 0px; text-align:right;}
 #active{font-weight:bold; font-size:16px;}
-.title{ font-weight:bold; margin:10px 0px 5px 0px;}
+.title{font-weight:bold; margin:10px 0px 5px 0px;}
 .title a:link, a:active,a:visited,a:hover{color:#333360; text-decoration:none}
-.info{ font-size:12px;color:#999999;}
-.info2{ font-size:12px; border-bottom:#CCCCCC dotted 1px; text-align:right; color:#666666; margin:5px 0px; padding:5px;}
-.posttitle{ font-size:16px; color:#333; font-weight:bold;}
-.postinfo{ font-size:12px; color: #999999;}
-.postcont{ border-bottom:1px solid #DDDDDD; padding:12px 0px; margin-bottom:10px;}
-.t{font-size:16px; font-weight:bold;}
-.c{ padding:10px;}
+.info{font-size:12px;color:#999999;}
+.info2{font-size:12px; border-bottom:#CCCCCC dotted 1px; text-align:right; color:#666666; margin:5px 0px; padding:5px;}
+.posttitle{font-size:16px; color:#333; font-weight:bold;}
+.postinfo{font-size:12px; color: #999999;}
+.postcont{border-bottom:1px solid #DDDDDD; padding:12px 0px; margin-bottom:10px;}
+.t{font-size:16px; font-weight:bold;}.c{padding:10px;}
 .l{border-bottom:1px solid #DDDDDD; padding:10px 0px;}
-.twcont{ color:#333; padding-top:12px;}
+.twcont{color:#333; padding-top:12px;}
 .twinfo{text-align:right; color:#999999; border-bottom:1px solid #DDDDDD; padding:8px 0px; font-size:12px;}
-.comcont{color:#333; padding:6px 0px;}
-.reply{ color:#FF3300; font-size:12px;}
+.comcont{color:#333; padding:6px 0px;}.reply{color:#FF3300; font-size:12px;}
 .cominfo{text-align:right; color:#999999; border-bottom:1px solid #DDDDDD; padding:8px 0px;font-size:12px;}
-.texts{ width:92%; height:200px;}
+.texts{width:92%; height:200px;}
 </style>
 </head>
 <body>
