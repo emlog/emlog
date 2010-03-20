748675671fb0dea8e7fc0d9ba257d7ec6607af87
emloog@gmail.com <emloog@gmail.com@5f219224-4642-0410-9343-2d18db063b08>
2010-03-20 04:14:59 +0000

    修改模板分页输出标签，让css可以方便的控制所有元素
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1615 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/lib/function.base.php
+++ src/lib/function.base.php
@@ -215,8 +215,8 @@ function pagination($count,$perlogs,$page,$url){
 			}
 		}
 	}
-	if ($page > 6) $re = "<a href=\"$url=1\" title=\"首页\">&laquo;</a> ...$re";
-	if ($page + 5 < $pnums) $re .= "... <a href=\"$url=$pnums\" title=\"尾页\">&raquo;</a>";
+	if ($page > 6) $re = "<a href=\"$url=1\" title=\"首页\">&laquo;</a><em>...</em>$re";
+	if ($page + 5 < $pnums) $re .= "<em>...</em> <a href=\"$url=$pnums\" title=\"尾页\">&raquo;</a>";
 	if ($pnums <= 1) $re = '';
 	return $re;
 }
