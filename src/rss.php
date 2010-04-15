3fd7b9127fcac6f0fcd17419f5ecb11079ba23fc
emloog@gmail.com <emloog@gmail.com@5f219224-4642-0410-9343-2d18db063b08>
2010-04-15 14:00:37 +0000

    修改rss输出顺序：以时间排序
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1648 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/rss.php
+++ src/rss.php
@@ -74,7 +74,7 @@ function GetBlog($sort = null)
 {
 	global $DB,$URL;
 	$subsql = $sort ? "and sortid=$sort" : '';
-	$sql = "SELECT * FROM ".DB_PREFIX."blog  WHERE hide='n' and type='blog' $subsql ORDER BY gid DESC limit 0,10";
+	$sql = "SELECT * FROM ".DB_PREFIX."blog  WHERE hide='n' and type='blog' $subsql ORDER BY date DESC limit 0,10";
 	$result = $DB->query($sql);
 	$blog = array();
 	while ($re = $DB->fetch_array($result))
