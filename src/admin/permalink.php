cfe4822015c3aa228f3ee16f799c9bcae1e449ee
emloog@gmail.com <emloog@gmail.com@5f219224-4642-0410-9343-2d18db063b08>
2010-05-31 16:43:37 +0000

    复辟了3.4的固定连接设置方式。设置时写到.htaccess文件里。
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1729 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/permalink.php
+++ src/admin/permalink.php
@@ -21,6 +21,43 @@ if ($action == '') {
 
 if ($action == 'update') {
 	$permalink = isset($_POST['permalink']) ? addslashes($_POST['permalink']) : '0';
+
+	if($permalink != '0'){
+		$fp = @fopen(EMLOG_ROOT.'/.htaccess', 'w');
+		$t = parse_url(BLOG_URL);
+		switch ($permalink){
+			case '1':
+				$rw_rule = '
+							<IfModule mod_rewrite.c>
+								RewriteEngine on
+								RewriteCond %{REQUEST_FILENAME} !-f
+								RewriteCond %{REQUEST_FILENAME} !-d
+								RewriteBase '.$t['path'].'
+								RewriteRule ^(post|record|sort|author|page)-([0-9]+)\.html$ index.php?$1=$2
+								RewriteRule ^tag-(.+)\.html$ index.php?tag=$1
+								RewriteRule ^t/page-([0-9]+)\.html$ t/index.php?page=$1
+							</IfModule>';
+				break;
+			case '2':
+				$rw_rule = '
+							<IfModule mod_rewrite.c>
+								RewriteEngine on
+								RewriteCond %{REQUEST_FILENAME} !-f
+								RewriteCond %{REQUEST_FILENAME} !-d
+								RewriteBase '.$t['path'].'
+								RewriteRule ^(post|record|sort|author|page)/([0-9]+)$ index.php?$1=$2
+								RewriteRule ^tag/(.+)$ index.php?tag=$1
+								RewriteRule ^t/page/([0-9]+)$ t/index.php?page=$1
+							</IfModule>';
+				break;
+		}
+		if (!@fwrite($fp, $rw_rule)){
+			header("Location: ./permalink.php?error=true");
+			exit;
+		}
+		fclose($fp);
+	}
+
 	updateOption('isurlrewrite', $permalink);
 	$CACHE->updateCache('options');
 	header("Location: ./permalink.php?activated=true");
