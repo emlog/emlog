f74626627057df7bc4701c9d3596363740de0bd0
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2011-02-04 14:30:22 +0000

    #4.0 dev 后台自定义模板,去除后台模板default。
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1823 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/default/footer.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
-</td></tr></tbody></table>
-<div><p align="center">&copy;2010 <a href="http://www.emlog.net" target="_blank">emlog</a></p></div>
-</div>
-</body>
-</html>
