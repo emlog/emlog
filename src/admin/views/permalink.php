9c0795bc09b9ca2c1f7023d3eb93cd457d70d1eb
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2011-04-03 03:52:28 +0000

    #4.0 dev 修改日志链接设置文案
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1883 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/permalink.php
+++ src/admin/views/permalink.php
@@ -9,8 +9,8 @@
 <?php if(isset($_GET['error'])):?><span class="error">保存失败：根目录下的.htaccess不可写</span><?php endif;?>
 </div>
 <div style="margin-left:10px;">
-<div class="des">你可以在这里修改日志链接的形式，这或许可以提高链接的可读性和对搜索引擎的友好程度。<br />
-如果修改后日志无法访问，那可能是你的服务器环境不支持URL重写，请修改回默认形式、关闭日志连接别名。启用链接别名后可以自定义日志和页面的链接地址。
+<div class="des">你可以在这里修改日志链接的形式，如果修改后日志无法访问，那可能是你的服务器环境不支持URL重写，请修改回默认形式、关闭日志连接别名。
+<br />启用链接别名后可以自定义日志和页面的链接地址。
 </div>
 <form action="permalink.php?action=update" method="post">
 <div style="margin:10px 10px;">
@@ -19,8 +19,8 @@
     <li><input type="radio" name="permalink" value="2" <?php echo $ex2; ?>>目录形式：<span class="permalink_url"><?php echo BLOG_URL; ?>post/1</span></li>
 	<li><input type="radio" name="permalink" value="3" <?php echo $ex3; ?>>分类形式：<span class="permalink_url"><?php echo BLOG_URL; ?>category/1.html</span></li>
     <div style="border-top:1px solid #F7F7F7; width:521px; margin:10px 0px 10px 0px;"></div>
-	<li>启用链接别名：<input type="checkbox" style="vertical-align:middle;" value="y" name="isalias" id="isalias" <?php echo $isalias; ?> /></li>
-	<li>启用链接别名html后缀：<input type="checkbox" style="vertical-align:middle;" value="y" name="isalias_html" id="isalias_html" <?php echo $isalias_html; ?> /></li>
+	<li>启用日志链接别名：<input type="checkbox" style="vertical-align:middle;" value="y" name="isalias" id="isalias" <?php echo $isalias; ?> /></li>
+	<li>启用日志链接别名html后缀：<input type="checkbox" style="vertical-align:middle;" value="y" name="isalias_html" id="isalias_html" <?php echo $isalias_html; ?> /></li>
 	<li style="margin-top:10px;"><input type="submit" value="保存设置" class="button" /></li>
 </div>
 </form>
