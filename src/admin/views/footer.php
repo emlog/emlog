a7a550ac655533897b1e3f170811e869f194f995
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2011-04-05 10:21:57 +0000

    #4.0 dev 修改版权年份
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1896 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/footer.php
+++ src/admin/views/footer.php
@@ -1,6 +1,6 @@
 <?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
 </td></tr></tbody></table>
-<div><p align="center">&copy;2010 <a href="http://www.emlog.net" target="_blank">emlog</a></p></div>
+<div><p align="center">&copy;2011 <a href="http://www.emlog.net" target="_blank">emlog</a></p></div>
 </div>
 </body>
 </html>
