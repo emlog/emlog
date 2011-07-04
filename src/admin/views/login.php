e0442090fe531210a1273d85e0ddc5ae26fd8902
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2011-07-04 14:57:03 +0000

    #4.1.0 dev 修复模板文件直接访问报错。
    git-svn-id: http://emlog.googlecode.com/svn/trunk@1992 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/admin/views/login.php
+++ src/admin/views/login.php
@@ -1,3 +1,4 @@
+<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
