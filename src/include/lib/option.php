ec4bc09615d87e1ecc84cc68ea3d8de72f942bc5
emloog <emloog@5f219224-4642-0410-9343-2d18db063b08>
2011-09-17 02:33:56 +0000

    #4.1.0 dev 修复url附带其他未知参数报404的bug
    git-svn-id: http://emlog.googlecode.com/svn/trunk@2034 5f219224-4642-0410-9343-2d18db063b08
    
    --HG--
    branch : _bitbucket

--- src/include/lib/option.php
+++ src/include/lib/option.php
@@ -55,11 +55,6 @@ class Option {
     static function getRoutingTable(){
     	$routingtable = array(
                     array(
-                            'model' => 'Log_Controller',
-                            'method' => 'display',
-                            'reg_0' => '|^/?$|',
-                            ),
-                    array(
                             'model' => 'calendar',
                             'method' => 'generate',
                             'reg_0' => '|^.*/\?action=cal|',
@@ -67,60 +62,65 @@ class Option {
                     array(
                             'model' => 'Log_Controller',
                             'method' => 'displayContent',
-                            'reg_0' => '|^.*/\?(post)=(\d+)(&comment-page=(\d+))?$|',
-                            'reg_1' => '|^.*/(post)-(\d+)\.html(/comment-page-(\d+))?/?$|',
-                            'reg_2' => '|^.*/(post)/(\d+)(/comment-page-(\d+))?/?$|',
-                    		'reg_3' => '|^/([^\./\?=]+)(\.html)?(/comment-page-(\d+))?/?$|',
+                            'reg_0' => '|^.*/\?(post)=(\d+)(&comment-page=(\d+))?|',
+                            'reg_1' => '|^.*/(post)-(\d+)\.html(/comment-page-(\d+))?/?|',
+                            'reg_2' => '|^.*/(post)/(\d+)(/comment-page-(\d+))?/?|',
+                    		'reg_3' => '|^/([^\./\?=]+)(\.html)?(/comment-page-(\d+))?/?|',
                             ),
                     array(
                             'model' => 'Record_Controller',
                             'method' => 'display',
-                            'reg_0' => '|^.*/\?(record)=(\d{6,8})(&(page)=(\d+))?$|',
-                            'reg' => '|^.*/(record)/(\d{6,8})/?((page)/(\d+))?/?$|',
+                            'reg_0' => '|^.*/\?(record)=(\d{6,8})(&(page)=(\d+))?|',
+                            'reg' => '|^.*/(record)/(\d{6,8})/?((page)/(\d+))?/?|',
                             ),
                     array(
                             'model' => 'Sort_Controller',
                             'method' => 'display',
-                            'reg_0' => '|^.*/\?(sort)=(\d+)(&(page)=(\d+))?$|',
-                            'reg' => '|^.*/(sort)/([^\./]+)/?((page)/(\d+))?/?$|',
+                            'reg_0' => '|^.*/\?(sort)=(\d+)(&(page)=(\d+))?|',
+                            'reg' => '|^.*/(sort)/([^\./]+)/?((page)/(\d+))?/?|',
                             ),
                     array(
                             'model' => 'Tag_Controller',
                             'method' => 'display',
-                            'reg_0' => '|^.*/\?(tag)=([^&]+)(&(page)=(\d+))?$|',
-                            'reg' => '|^.*/(tag)/([^/]+)/?((page)/(\d+))?/?$|',
+                            'reg_0' => '|^.*/\?(tag)=([^&]+)(&(page)=(\d+))?|',
+                            'reg' => '|^.*/(tag)/([^/]+)/?((page)/(\d+))?/?|',
                             ),
                     array(
                             'model' => 'Author_Controller',
                             'method' => 'display',
-                            'reg_0' => '|^.*/\?(author)=(\d+)(&(page)=(\d+))?$|',
-                            'reg' => '|^.*/(author)/(\d+)/?((page)/(\d+))?/?$|',
+                            'reg_0' => '|^.*/\?(author)=(\d+)(&(page)=(\d+))?|',
+                            'reg' => '|^.*/(author)/(\d+)/?((page)/(\d+))?/?|',
                             ),
                     array(
                             'model' => 'Log_Controller',
                             'method' => 'display',
-                            'reg_0' => '|^.*/\?(page)=(\d+)$|',
-                            'reg' => '|^.*/(page)/(\d+)/?$|',
+                            'reg_0' => '|^.*/\?(page)=(\d+)|',
+                            'reg' => '|^.*/(page)/(\d+)/?|',
                             ),
                     array(
                             'model' => 'Search_Controller',
                             'method' =>'display',
-                            'reg_0' => '|^.*/\?(keyword)=([^/&]+)(&(page)=(\d+))?$|',
+                            'reg_0' => '|^.*/\?(keyword)=([^/&]+)(&(page)=(\d+))?|',
                             ),
                     array(
                             'model' => 'Comment_Controller',
                             'method' => 'addComment',
-                            'reg_0' => '|^.*/\?(action)=(addcom)$|',
+                            'reg_0' => '|^.*/\?(action)=(addcom)|',
                             ),
                     array(
                             'model' => 'Plugin_Controller',
                             'method' => 'loadPluginShow',
-                            'reg_0' => '|^.*/\?(plugin)=([\w\-]+).*$|',
+                            'reg_0' => '|^.*/\?(plugin)=([\w\-]+).*|',
                             ),
                     array(
                             'model' => 'Log_Controller',
                             'method' => 'displayContent',
-                            'reg_0' => '|^.*?/([^/\.=\?]+)(\.html)?(/comment-page-(\d+))?/?$|',
+                            'reg_0' => '|^.*?/([^/\.=\?]+)(\.html)?(/comment-page-(\d+))?/?|',
+                            ),
+                    array(
+                            'model' => 'Log_Controller',
+                            'method' => 'display',
+                            'reg_0' => '|^/?|',
                             ),
                             
                 );
