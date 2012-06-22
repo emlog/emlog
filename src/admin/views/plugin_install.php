dab2836cf958df00cbb4cc3e05bc943b879f2f67
emloog <none@none>
2012-06-22 23:35:13 +0800

    #4.3.0# 优化默认设置及界面细节
    --HG--
    branch : _bitbucket

--- src/admin/views/plugin_install.php
+++ src/admin/views/plugin_install.php
@@ -17,7 +17,7 @@
 </div>
 </div>
 <?php endif; ?>
-<div style="margin:20px 10px;">
+<div style="margin:20px 22px;">
 <div class="des">请上传一个zip压缩格式的插件安装包。<a href="http://www.emlog.net/plugins/" target="_blank">获得更多插件&raquo;</a></div>
 </div>
 <form action="./plugin.php?action=upload_zip" method="post" enctype="multipart/form-data" >
