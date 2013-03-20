100e523e70d4899a347d79602b9ce1f29e3175ab
dawei <none@none>
2013-03-20 17:34:19 +0800

    #5.0.1#搜索未找到时增加提示
    --HG--
    branch : _bitbucket

--- src/content/templates/default/log_list.php
+++ src/content/templates/default/log_list.php
@@ -7,7 +7,11 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 <div id="content">
 <div id="contentleft">
 <?php doAction('index_loglist_top'); ?>
-<?php foreach($logs as $value): ?>
+
+<?php 
+if (!empty($logs)):
+foreach($logs as $value): 
+?>
 	<h2><?php topflg($value['top']); ?><a href="<?php echo $value['log_url']; ?>"><?php echo $value['log_title']; ?></a></h2>
 	<p class="date">作者：<?php blog_author($value['author']); ?> 发布于：<?php echo gmdate('Y-n-j G:i l', $value['date']); ?> 
 	<?php blog_sort($value['logid']); ?> 
@@ -21,7 +25,13 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 	<a href="<?php echo $value['log_url']; ?>">浏览(<?php echo $value['views']; ?>)</a>
 	</p>
 	<div style="clear:both;"></div>
-<?php endforeach; ?>
+<?php 
+endforeach;
+else:
+?>
+	<h2>未找到</h2>
+	<p>抱歉，没有符合您查询条件的结果。</p>
+<?php endif;?>
 
 <div id="pagenavi">
 	<?php echo $page_url;?>
