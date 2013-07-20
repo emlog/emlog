8a4aa27cad799c0a42e94c11564cd489f39c832d
dawei <emloog@gmail.com>
2013-07-20 19:56:34 +0800

    修复xmrpc验证漏洞

--- src/xmlrpc.php
+++ src/xmlrpc.php
@@ -594,7 +594,7 @@ function login($username, $password) {
 	$username = addslashes($username);
 	$password = addslashes($password);
 	// 检查用户权限
-	if (!LoginAuth::checkUser($username, $password , '', 'n')) {
+	if (true !== LoginAuth::checkUser($username, $password , '', 'n')) {
 		error_message(403, '用户名密码错误');
 		return false;
 	}
