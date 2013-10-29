6d53083f0fecb2c0e0e06a08079ea8cd522e0973
Colt Xie <colt.xie@gmail.com>
2013-10-29 17:12:00 +0800

    删除config.php文件，增加config.sample.php，方便开发 添加.gitignore文件自动过滤相关文件

---
+++ src/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+//mysql database address
+define('DB_HOST','127.0.0.1');
+//mysql database user
+define('DB_USER','root');
+//database password
+define('DB_PASSWD','');
+//database name
+define('DB_NAME','emlog');
+//database prefix
+define('DB_PREFIX','emlog_');
+//auth key
+define('AUTH_KEY','emlog-key');
+//cookie name
+define('AUTH_COOKIE_NAME','emlog-cookie');
