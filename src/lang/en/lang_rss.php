5d75b9cb1c9ce09bbf8bc72b32244ca33a6972c7
Valery Votintsev <vvotintsev@gmail.com>
2014-02-17 20:00:46 +0300

    Fix for en/lang_install, en/lang_rss
    --HG--
    branch : _multilang

---
+++ src/lang/en/lang_rss.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang = array(
+
+//---------------------------
+//rss.php
+/*[80]*/  'post_encrypted' => '[This article has been set encrypted]',//'[该文章已设置加密]',
+//[89]*/  'read_more' => 'Read more';//'阅读全文',
+
+);
