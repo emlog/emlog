528f9afbe4bfe357561211cfc6c981d6d11df691
codersclub <vvotintsev@gmail.com>
2014-07-03 15:56:16 +0300

    Config.sample unified. Version number added into the footer.
    --HG--
    branch : _multilang

--- src/config.sample.php
+++ src/config.sample.php
@@ -1,21 +1,30 @@
 <?php
 header("location: ./install.php");exit;
+
 //mysql database address
-define('DB_HOST','127.0.0.1');
+define('DB_HOST','127.0.0.1');// or 'localhost'
+
 //mysql database user
 define('DB_USER','root');
+
 //database password
 define('DB_PASSWD','');
+
 //database name
 define('DB_NAME','emlog');
+
 //database prefix
 define('DB_PREFIX','emlog_');
+
 //auth key
 define('AUTH_KEY','emlog-key');
+
 //cookie name
 define('AUTH_COOKIE_NAME','emlog-cookie');
+
 //blog language //vot
-define('EMLOG_LANGUAGE','en'); //'zh-CN', 'ru'
+define('EMLOG_LANGUAGE','en'); //'en', 'ru', 'sc', 'tc', etc.
+
 //Version number //vot
 define('EMLOG_VERSION','5.3.0');
 
--- src/content/templates/default/footer.php
+++ src/content/templates/default/footer.php
@@ -7,7 +7,7 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 </div><!--end #content-->
 <div style="clear:both;"></div>
 <div id="footerbar">
-<!--vot--> <?=lang('powered_by')?> <a href="http://www.emlog.net" title="<?=lang('powered_by_emlog')?>">emlog</a> 
+<!--vot--> <?=lang('powered_by')?> <a href="http://www.emlog.net" title="<?=lang('powered_by_emlog')?>">emlog</a> v.<?=EMLOG_VERSION?>
 <!--vot--> <? if($icp) {?><a href="http://www.miibeian.gov.cn" target="_blank"><?= $icp; ?></a> <?}?><?=$footer_info?>
 	<?php doAction('index_footer'); ?>
 </div><!--end #footerbar-->
