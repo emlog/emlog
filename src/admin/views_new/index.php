c3404c8e60e8f834ca15c32c4d9fa595c0814fb4
dawei <emloog@gmail.com>
2014-07-23 17:26:14 +0800

    the next em 6

---
+++ src/admin/views_new/index.php
@@ -0,0 +1,12 @@
+<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Blank</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
