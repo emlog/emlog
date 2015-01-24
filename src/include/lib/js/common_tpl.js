01b7dd81b0e74a50025e31e9eced79bdb1186b5b
Fieah <im@fieah.com>
2015-01-25 03:11:20 +0800

    common_tpl.js: hmm...

--- src/include/lib/js/common_tpl.js
+++ src/include/lib/js/common_tpl.js
@@ -1,53 +1,58 @@
 function id(id) {
 	return document.getElementById(id) || null;
 }
-function focusEle(ele){
-	try {id(ele).focus();}
-	catch(e){}
+
+function focusEle(ele) {
+	try {
+		id(ele).focus();
+	} catch(e) {}
 }
-function updateEle(ele,content){
+
+function updateEle(ele, content) {
 	id(ele).innerHTML = content;
 }
-function timestamp(){
+
+function timestamp() {
 	return new Date().getTime();
 }
+
 var XMLHttp = {
 	_objPool: [],
-	_getInstance: function () {
-		for (var i = 0; i < this._objPool.length; i ++) {
-			if (this._objPool[i].readyState == 0 || this._objPool[i].readyState == 4) {
+	_getInstance: function() {
+		for(var i = 0; i < this._objPool.length; i++) {
+			if(this._objPool[i].readyState == 0 || this._objPool[i].readyState == 4) {
 				return this._objPool[i];
 			}
 		}
 		this._objPool[this._objPool.length] = this._createObj();
 		return this._objPool[this._objPool.length - 1];
 	},
-	_createObj: function(){
-		if (window.XMLHttpRequest){
+	_createObj: function() {
+		if(window.XMLHttpRequest) {
 			var objXMLHttp = new XMLHttpRequest();
 		} else {
 			var MSXML = ['MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0', 'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP'];
-			for(var n = 0; n < MSXML.length; n ++){
-				try{
+			for(var n = 0; n < MSXML.length; n++) {
+				try {
 					var objXMLHttp = new ActiveXObject(MSXML[n]);
 					break;
-				}catch(e){}
+				} catch(e) {}
 			}
 		}
-		if (objXMLHttp.readyState == null){
+		if(objXMLHttp.readyState === null) {
 			objXMLHttp.readyState = 0;
-			objXMLHttp.addEventListener('load',function(){
+			objXMLHttp.addEventListener('load', function() {
 				objXMLHttp.readyState = 4;
-				if (typeof objXMLHttp.onreadystatechange == "function") {
+				if(typeof objXMLHttp.onreadystatechange == 'function') {
 					objXMLHttp.onreadystatechange();
 				}
 			}, false);
 		}
 		return objXMLHttp;
 	},
-	sendReq: function(method, url, data, callback){
+	sendReq: function(method, url, data, callback) {
 		var objXMLHttp = this._getInstance();
-		with(objXMLHttp){
+		with(objXMLHttp) {
 			try {
 				if (url.indexOf("?") > 0) {
 					url += "&randnum=" + Math.random();
@@ -68,33 +73,41 @@ var XMLHttp = {
 		}
 	}
 };
-function sendinfo(url,node){
-	updateEle(node,"<div><span style=\"background-color:#FFFFE5; color:#666666;\">加载中...</span></div>");
-	XMLHttp.sendReq('GET',url,'',function(obj){updateEle(node,obj.responseText);});
+
+function sendinfo(url, node) {
+	updateEle(node, '<div><span style="background-color:#FFFFE5; color:#666666;">加载中...</span></div>');
+	XMLHttp.sendReq('GET', url, '', function(obj) {
+		updateEle(node, obj.responseText);
+	});
 }
-function loadr(url,tid){
-	url = url+"&stamp="+timestamp();
-	var r=id("r_"+tid),
-		rp=id("rp_"+tid);
-	if (r.style.display=="block"){
-		r.style.display="none";
-		rp.style.display="none";
+
+function loadr(url, tid) {
+	url = url + '&stamp=' + timestamp();
+	var r = id('r_' + tid),
+		rp = id('rp_' + tid);
+	if(r.style.display == 'block') {
+		r.style.display = 'none';
+		rp.style.display = 'none';
 	} else {
-		r.style.display="block";
-		r.innerHTML = '<span style=\"background-color:#FFFFE5;text-align:center;font-size:12px;color:#666666;\">加载中...</span>';
-		XMLHttp.sendReq('GET',url,'',function(obj){r.innerHTML = obj.responseText;rp.style.display="block";});
+		r.style.display = 'block';
+		r.innerHTML = '<span style="background-color:#FFFFE5;text-align:center;font-size:12px;color:#666666;">加载中...</span>';
+		XMLHttp.sendReq('GET', url, '', function(obj) {
+			r.innerHTML = obj.responseText;
+			rp.style.display = 'block';
+		});
 	}
 }
-function reply(url,tid){
-	var rtext=id("rtext_"+tid).value,
-		rname=id("rname_"+tid).value,
-		rcode=id("rcode_"+tid).value,
-		rmsg=id("rmsg_"+tid),
-		rn=id("rn_"+tid),
+
+function reply(url, tid) {
+	var rtext = id('rtext_' + tid).value,
+		rname = id('rname_' + tid).value,
+		rcode = id('rcode_' + tid).value,
+		rmsg = id('rmsg_' + tid),
+		rn = id('rn_' + tid),
 		rp = '',
-		r=id("r_"+tid),
-		data = "r="+rtext+"&rname="+rname+"&rcode="+rcode+"&tid="+tid;
-	XMLHttp.sendReq('POST',url,data,function(obj){
+		r = id('r_' + tid),
+		data = 'r=' + rtext + '&rname=' + rname + '&rcode=' + rcode + '&tid=' + tid;
+	XMLHttp.sendReq('POST', url, data, function(obj) {
 		switch(obj.responseText) {
 			case 'err1':
 				rp = '(回复长度需在140个字内)';
@@ -124,18 +137,22 @@ function reply(url,tid){
 		}
 		rmsg.innerHTML = rp;
 }
-function re(tid, rp){
-	var rtext=id("rtext_"+tid).value = rp;
-	focusEle("rtext_"+tid);
+
+function re(tid, rp) {
+	var rtext=id('rtext_' + tid).value = rp;
+	focusEle('rtext_' + tid);
 }
-function commentReply(pid,c){
+
+function commentReply(pid, c) {
 	var response = id('comment-post');
 	id('comment-pid').value = pid;
 	id('cancel-reply').style.display = '';
 	c.parentNode.parentNode.appendChild(response);
 }
-function cancelReply(){
-	var commentPlace = id('comment-place'),response = id('comment-post');
+
+function cancelReply() {
+	var commentPlace = id('comment-place'),
+		response = id('comment-post');
 	id('comment-pid').value = 0;
 	id('cancel-reply').style.display = 'none';
 	commentPlace.appendChild(response);
