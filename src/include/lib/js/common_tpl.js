914d98201d912143eff2f8ba7c01b2f26289fde3
Fieah <im@fieah.com>
2015-01-25 06:47:13 +0800

    common_tpl.js: XMLHttp & Hotfix for reply()
    Ooops.
    Now everything's fine.

--- src/include/lib/js/common_tpl.js
+++ src/include/lib/js/common_tpl.js
@@ -17,58 +17,23 @@ function timestamp() {
 }
 
 var XMLHttp = {
-	_objPool: [],
-	_getInstance: function() {
-		for(var i = 0; i < this._objPool.length; i++) {
-			if(this._objPool[i].readyState == 0 || this._objPool[i].readyState == 4) {
-				return this._objPool[i];
-			}
-		}
-		this._objPool[this._objPool.length] = this._createObj();
-		return this._objPool[this._objPool.length - 1];
-	},
 	_createObj: function() {
 		if(window.XMLHttpRequest) {
-			var objXMLHttp = new XMLHttpRequest();
-		} else {
-			var MSXML = ['MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0', 'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP'];
-			for(var n = 0; n < MSXML.length; n++) {
-				try {
-					var objXMLHttp = new ActiveXObject(MSXML[n]);
-					break;
-				} catch(e) {}
-			}
+			return new XMLHttpRequest();
+		} else if(window.ActiveXObject) {
+			return new ActiveXObject('Microsoft.XMLHTTP');
 		}
-		if(objXMLHttp.readyState === null) {
-			objXMLHttp.readyState = 0;
-			objXMLHttp.addEventListener('load', function() {
-				objXMLHttp.readyState = 4;
-				if(typeof objXMLHttp.onreadystatechange == 'function') {
-					objXMLHttp.onreadystatechange();
-				}
-			}, false);
-		}
-		return objXMLHttp;
+		return false;
 	},
 	sendReq: function(method, url, data, callback) {
-		var objXMLHttp = this._getInstance();
-		with(objXMLHttp) {
-			try {
-				if (url.indexOf("?") > 0) {
-					url += "&randnum=" + Math.random();
-				} else {
-					url += "?randnum=" + Math.random();
-				}
-				open(method, url, true);
-				setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-				send(data);
-				onreadystatechange = function () {
-					if (objXMLHttp.readyState == 4 && (objXMLHttp.status == 200 || objXMLHttp.status == 304)) {
-						callback(objXMLHttp);
-					}
-				}
-			} catch(e) {
-				alert('emria:error');
+		var objXMLHttp = this._createObj();
+		//url += (url.indexOf('?') > 0 ? '&' : '?') + 'randnum=' + Math.random();
+		objXMLHttp.open(method, url);
+		objXMLHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		objXMLHttp.send(data);
+		objXMLHttp.onreadystatechange = function() {
+			if(objXMLHttp.readyState == 4 && objXMLHttp.status == 200) {
+				callback(objXMLHttp);
 			}
 		}
 	}
@@ -136,6 +101,7 @@ function reply(url, tid) {
 				break;
 		}
 		rmsg.innerHTML = rp;
+	});
 }
 
 function re(tid, rp) {
