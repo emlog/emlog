628b1d15a0b5dfa60c991488af7f6394ed323fa4
Fieah <im@fieah.com>
2015-02-08 15:54:38 +0800

    common_tpl.js: Ajax优化

--- src/include/lib/js/common_tpl.js
+++ src/include/lib/js/common_tpl.js
@@ -1,5 +1,5 @@
 function $Id(id) {
-	return $Id(id) || null;
+	return document.getElementById(id) || null;
 }
 
 function focusEle(ele){
@@ -13,58 +13,14 @@ function timestamp(){
 	return new Date().getTime();
 }
 var XMLHttp = {
-	_objPool: [],
-	_getInstance: function () {
-		for (var i = 0; i < this._objPool.length; i ++) {
-			if (this._objPool[i].readyState == 0 || this._objPool[i].readyState == 4) {
-				return this._objPool[i];
-			}
-		}
-		this._objPool[this._objPool.length] = this._createObj();
-		return this._objPool[this._objPool.length - 1];
-	},
-	_createObj: function(){
-		if (window.XMLHttpRequest){
-			var objXMLHttp = new XMLHttpRequest();
-		} else {
-			var MSXML = ['MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0', 'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP'];
-			for(var n = 0; n < MSXML.length; n ++){
-				try{
-					var objXMLHttp = new ActiveXObject(MSXML[n]);
-					break;
-				}catch(e){}
-			}
-		}
-		if (objXMLHttp.readyState == null){
-			objXMLHttp.readyState = 0;
-			objXMLHttp.addEventListener('load',function(){
-				objXMLHttp.readyState = 4;
-				if (typeof objXMLHttp.onreadystatechange == "function") {
-					objXMLHttp.onreadystatechange();
-				}
-			}, false);
-		}
-		return objXMLHttp;
-	},
-	sendReq: function(method, url, data, callback){
-		var objXMLHttp = this._getInstance();
-		with(objXMLHttp){
-			try {
-				if (url.indexOf("?") > 0) {
-					url += "&randnum=" + Math.random();
-				} else {
-					url += "?randnum=" + Math.random();
-				}
-				open(method, url, true);
-				setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-				send(data);
-				onreadystatechange = function () {
-					if (objXMLHttp.readyState == 4 && (objXMLHttp.status == 200 || objXMLHttp.status == 304)) {
-						callback(objXMLHttp);
-					}
-				}
-			} catch(e) {
-				alert('emria:error');
+	sendReq: function(method, url, data, callback) {
+		var objXMLHttp = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP') || null;
+		objXMLHttp.open(method, url);
+		objXMLHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		objXMLHttp.send(data);
+		objXMLHttp.onreadystatechange = function() {
+			if(objXMLHttp.readyState == 4 && objXMLHttp.status == 200) {
+				callback(objXMLHttp);
 			}
 		}
 	}
