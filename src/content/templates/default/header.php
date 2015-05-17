b855fda9282251f2d958504d314b9ceb8a6ecd24
emlog <emloog@gmail.com>
2015-05-17 18:41:29 +0800

    修复首页响应式bug

--- src/content/templates/default/header.php
+++ src/content/templates/default/header.php
@@ -8,14 +8,15 @@ Author Url:http://www.emlog.net
 if(!defined('EMLOG_ROOT')) {exit('error!');}
 require_once View::getView('module');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php echo $site_title; ?></title>
 <meta name="keywords" content="<?php echo $site_key; ?>" />
 <meta name="description" content="<?php echo $site_description; ?>" />
 <meta name="generator" content="emlog" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="alternate" type="application/rss+xml" title="RSS"  href="<?php echo BLOG_URL; ?>rss.php" />
 <link href="<?php echo TEMPLATE_URL; ?>main.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo BLOG_URL; ?>admin/views/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
