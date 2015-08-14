4d240128f0e27b717f8618e4cd8c9690a6d9a667
vibbow <vibbow@hotmail.com>
2015-08-14 01:04:22 +0100

    初始化mbstring模块的内部编码。

--- src/init.php
+++ src/init.php
@@ -10,6 +10,10 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define('EMLOG_ROOT', dirname(__FILE__));
 
+if (extension_loaded('mbstring')) {
+	mb_internal_encoding('UTF-8');
+}
+
 require_once EMLOG_ROOT.'/config.php';
 require_once EMLOG_ROOT.'/include/lib/function.base.php';
 
