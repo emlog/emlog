661d639d0cd8f070108021955f447747d886411c
Valery Votintsev <vvotintsev@gmail.com>
2016-05-13 19:53:44 +0300

    Detect access with HTTPS
    --HG--
    branch : _multilang

--- src/init.php
+++ src/init.php
@@ -37,6 +37,7 @@ define('ROLE', ISLOGIN === true ? $userData['role'] : ROLE_VISITOR);
 //User ID
 define('UID', ISLOGIN === true ? $userData['uid'] : '');
 //Site fixed address
+/*vot*/define('SCHEME', isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://');
 define('BLOG_URL', Option::get('blogurl'));
 //Template Library URL
 define('TPLS_URL', BLOG_URL.'content/templates/');
