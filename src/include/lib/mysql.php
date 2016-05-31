77c69d4ce4056b91ce74e8d94065db694a103a2d
DXkite <DXkite@163.com>
2016-05-31 18:09:50 +0800

    修改数据库使用PDO
    只是初略的将MYSQL的调用修改成PDO调用,最好就是重写数据库部分

--- src/include/lib/mysql.php
+++ src/include/lib/mysql.php
@@ -1,30 +1,28 @@
 <?php
 /**
- * MySQL数据库操作类
- *
- * @copyright (c) Emlog All Rights Reserved
+ * MySQL数据库操作类-PDO  兼容接口
+ * For PHPv7 
+ * @copyright (c) DXkite All Rights Reserved
  */
 
+// 兼容MySQL PHP<7
+defined ('MYSQL_ASSOC') or define('MYSQL_ASSOC' ,PDO::FETCH_ASSOC);
+defined ('MYSQL_BOTH') or define('MYSQL_BOTH' ,PDO::FETCH_BOTH);
+defined ('MYSQL_NUM') or define('MYSQL_NUM' ,PDO::FETCH_NUM);
+
 class MySql {
+    /**
+    * PDO对象
+    * @var object
+    */
+    private $pdo=null;
+    private $stmt=null;
 
     /**
      * 查询次数
      * @var int
      */
     private $queryCount = 0;
-
-    /**
-     * 内部数据连接对象
-     * @var resourse
-     */
-    private $conn;
-
-    /**
-     * 内部数据结果
-     * @var resourse
-     */
-    private $result;
-
     /**
      * 内部实例对象
      * @var object MySql
@@ -32,12 +30,11 @@ class MySql {
     private static $instance = null;
 
     private function __construct() {
-        if (!function_exists('mysql_connect')) {
-            emMsg('服务器空间PHP不支持MySql数据库');
-        }
-        if (!$this->conn = @mysql_connect(DB_HOST, DB_USER, DB_PASSWD)) {
-            switch ($this->geterrno()) {
-                case 2005:
+        try {
+            $this->pdo=new PDO('mysql:dbname='.DB_NAME.';dbhost='.DB_HOST,DB_USER,DB_PASSWD);
+        } catch (PDOException $e) {
+            switch ($e->getCode()) {
+                 case 2005:
                     emMsg("连接数据库失败，数据库地址错误或者数据库服务器不可用");
                     break;
                 case 2003:
@@ -50,14 +47,10 @@ class MySql {
                     emMsg("连接数据库失败，数据库用户名或密码错误");
                     break;
                 default :
-                    emMsg("连接数据库失败，请检查数据库信息。错误编号：" . $this->geterrno());
+                    emMsg("连接数据库失败，请检查数据库信息。错误编号：". $e->getCode());
                     break;
             }
         }
-        if ($this->getMysqlVersion() > '4.1') {
-            mysql_query("SET NAMES 'utf8'");
-        }
-        @mysql_select_db(DB_NAME, $this->conn) OR emMsg("连接数据库失败，未找到您填写的数据库");
     }
 
     /**
@@ -74,7 +67,8 @@ class MySql {
      * 关闭数据库连接
      */
     function close() {
-        return mysql_close($this->conn);
+        $this->pdo=null;
+        return true;
     }
 
     /**
@@ -82,13 +76,15 @@ class MySql {
      *
      */
     function query($sql, $ignore_err = FALSE) {
-        $this->result = @mysql_query($sql, $this->conn);
+        $this->stmt =$this->pdo->query($sql);
         $this->queryCount++;
+        return $this->stmt;
+        /*
         if (!$ignore_err && !$this->result) {
             emMsg("SQL语句执行错误：$sql <br />" . $this->geterror());
         }else {
             return $this->result;
-        }
+        }*/
     }
 
     /**
@@ -96,12 +92,12 @@ class MySql {
      *
      */
     function fetch_array($query , $type = MYSQL_ASSOC) {
-        return mysql_fetch_array($query, $type);
+        return $this->stmt->fetch($type);
     }
 
     function once_fetch_array($sql) {
-        $this->result = $this->query($sql);
-        return $this->fetch_array($this->result);
+        $this->query($sql);
+        return $this->fetch_array(null);
     }
 
     /**
@@ -109,7 +105,7 @@ class MySql {
      *
      */
     function fetch_row($query) {
-        return mysql_fetch_row($query);
+        return $this->stmt->fetch(PDO::FETCH_NUM);
     }
 
     /**
@@ -117,48 +113,51 @@ class MySql {
      *
      */
     function num_rows($query) {
-        return mysql_num_rows($query);
+        //return mysql_num_rows($query);
+        return $this->stmt->rowCount();
     }
 
     /**
      * 取得结果集中字段的数目
      */
     function num_fields($query) {
-        return mysql_num_fields($query);
+        //return mysql_num_fields($query);
+        return $this->stmt->columnCount();
     }
     /**
      * 取得上一步 INSERT 操作产生的 ID
      */
     function insert_id() {
-        return mysql_insert_id($this->conn);
+        return $this->pdo->lastInsertId();
     }
 
     /**
      * 获取mysql错误
      */
     function geterror() {
-        return mysql_error();
+        return $this->stmt->errorInfo();
     }
 
     /**
      * 获取mysql错误编码
      */
     function geterrno() {
-        return mysql_errno();
+        return $this->stmt->errorCode();
     }
 
     /**
      * Get number of affected rows in previous MySQL operation
      */
     function affected_rows() {
-        return mysql_affected_rows();
+        return $this->stmt->rowCount();
+        //return mysql_affected_rows();
     }
 
     /**
      * 取得数据库版本信息
      */
     function getMysqlVersion() {
-        return mysql_get_server_info();
+        return  $this->pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
     }
 
     /**
@@ -169,9 +168,12 @@ class MySql {
     }
 
     /**
+     *  notice  :PDO中无需这个，被废弃了 
+     *  这里用QOUTE方法代替 
      *  Escapes special characters
      */
     function escape_string($sql) {
-        return mysql_real_escape_string($sql);
+        return $this->pdo->quote($sql);
+        ///return mysql_real_escape_string($sql);
     }
 }
