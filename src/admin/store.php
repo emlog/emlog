c925532a6c1489030d679f5df8bb2b2530333e0d
emlog <emlog@qq.com>
2018-10-01 10:22:24 +0800

    fix:install addon error

--- src/admin/store.php
+++ src/admin/store.php
@@ -35,7 +35,7 @@ if ($action == 'insplu') {
 }
 
 if ($action == 'addon') {
-	$source = isset($_GET['source']) ? trim($_GET['source']) : '';
+	$source = isset($_GET['source']) ? trim($_GET['source'],"/") : '';
 	$source_type = isset($_GET['type']) ? trim($_GET['type']) : '';
 	if (empty($source)) {
 		exit('error');
