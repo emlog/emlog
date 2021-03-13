43420086fde666dc2883846efdfb93bc57820427
snow <emlog@qq.com>
2021-03-13 17:14:04 +0800

    feat:for admin tpl
    Signed-off-by: snow <emlog@qq.com>

--- admin/views/write.php
+++ admin/views/write.php
@@ -3,7 +3,6 @@
 } ?>
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800"><?php echo $containertitle; ?></h1>
-    <span id="msg_2"></span>
     <form action="save_log.php?action=add" method="post" enctype="multipart/form-data" id="addlog" name="addlog">
         <!--文章内容-->
         <div class="row">
@@ -19,23 +18,17 @@
                             <iframe width="100%" height="330" frameborder="0" src="<?php echo $att_frame_url; ?>"></iframe>
                         </div>
                     </div>
-                    <div id="editor"></div>
-                    <!--                    <div>-->
-                    <!--                        <textarea id="logcontent" name="logcontent" style="width:100%; height:460px;">--><?php //echo $content; ?><!--</textarea>-->
-                    <!--                    </div>-->
-                    <div class="show_advset" onclick="displayToggle('advset', 1);">高级选项<i class="fa fa-caret-right fa-fw"></i></div>
+                    <textarea id="logcontent" name="logcontent""><?php echo $content; ?></textarea>
+                    <div class="show_advset" onclick="displayToggle('advset', 1);">文章摘要<i class="fa fa-caret-right fa-fw"></i></div>
                     <div id="advset">
-                        <div>文章摘要：</div>
-                        <div><textarea id="logexcerpt" name="logexcerpt" style="width:100%; height:260px;"><?php echo $excerpt; ?></textarea></div>
+                        <textarea id="logexcerpt" name="logexcerpt"><?php echo $excerpt; ?></textarea>
                     </div>
                 </div>
                 <div class=line></div>
             </div>
-
             <!--文章侧边栏-->
-            <div class="col-xl-4 container-side">
+            <div class="col-xl-4">
                 <div class="panel panel-default">
-                    <div class="panel-heading">设置项</div>
                     <div class="panel-body">
                         <div class="form-group">
                             <select name="sort" id="sort" class="form-control">
@@ -125,58 +118,25 @@
             </div>
         </div>
     </form>
-</div>
 
-
-<div class="modal fade bd-example-modal-lg" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <a class="btn btn-success fileinput-button dz-clickable"><i class="glyphicon glyphicon-plus"></i>选择文件上传...</a>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- HTML heavily inspired by http://blueimp.github.io/jQuery-File-Upload/ -->
-                <div class="table table-striped" class="files" id="previews">
-                    <div id="template" class="file-row">
-                        <!-- This is used as the file preview template -->
-                        <div>
-                            <span class="preview"><img data-dz-thumbnail/></span>
-                        </div>
-                        <div>
-                            <p class="name" data-dz-name></p>
-                            <strong class="error text-danger" data-dz-errormessage></strong>
-                        </div>
-                        <div>
-                            <p class="size" data-dz-size></p>
-                            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                                <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                            </div>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary start">
-                                <i class="glyphicon glyphicon-upload"></i>
-                                <span>Start</span>
-                            </button>
-                            <button data-dz-remove class="btn btn-warning cancel">
-                                <i class="glyphicon glyphicon-ban-circle"></i>
-                                <span>Cancel</span>
-                            </button>
-                            <button data-dz-remove class="btn btn-danger delete">
-                                <i class="glyphicon glyphicon-trash"></i>
-                                <span>Delete</span>
-                            </button>
-                        </div>
-                    </div>
+    <!--资源库-->
+    <div class="modal fade bd-example-modal-lg" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">资源库</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
 
                 </div>
 
             </div>
-
         </div>
     </div>
+
 </div>
 
 <script>
@@ -186,64 +146,46 @@
     $("#menu_write").addClass('active');
 
 
-    ClassicEditor
-        .create( document.querySelector( '#editor' ), {
-            toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
-            heading: {
-                options: [
-                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                ]
-            }
-        } )
-        .catch(error => {
-            console.error(error);
-        });
+
+
+    ClassicEditor.create(document.querySelector('#logcontent'), {
+        toolbar: {
+            items: [
+                'heading', '|',
+                'fontfamily', 'fontsize', '|',
+                'alignment', '|',
+                'fontColor', 'fontBackgroundColor', '|',
+                'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+                'link', '|',
+                'outdent', 'indent', '|',
+                'bulletedList', 'numberedList', 'todoList', '|',
+                'code', 'codeBlock', '|',
+                'insertTable', '|',
+                'uploadImage', 'blockQuote', '|',
+                'undo', 'redo'
+            ],
+            shouldNotGroupWhenFull: true
+        }
+    }).catch(error => {
+        console.error(error);
+    });
+
+    ClassicEditor.create(document.querySelector('#logexcerpt'), {
+        toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+        heading: {
+            options: [
+                {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+                {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+                {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'}
+            ]
+        }
+    }).catch(error => {
+        console.error(error);
+    });
 
 
     $("#alias").keyup(function () {
         checkalias();
     });
 
-    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-    var previewNode = document.querySelector("#template");
-    previewNode.id = "";
-    var previewTemplate = previewNode.parentNode.innerHTML;
-    previewNode.parentNode.removeChild(previewNode);
-
-    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-        url: "/target-url", // Set the url
-        thumbnailWidth: 80,
-        thumbnailHeight: 80,
-        parallelUploads: 20,
-        previewTemplate: previewTemplate,
-        autoQueue: false, // Make sure the files aren't queued until manually added
-        previewsContainer: "#previews", // Define the container to display the previews
-        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-    });
-
-    myDropzone.on("addedfile", function (file) {
-        // Hookup the start button
-        file.previewElement.querySelector(".start").onclick = function () {
-            myDropzone.enqueueFile(file);
-        };
-    });
-
-    // Update the total progress bar
-    myDropzone.on("totaluploadprogress", function (progress) {
-        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
-    });
-
-    myDropzone.on("sending", function (file) {
-        // Show the total progress bar when upload starts
-        document.querySelector("#total-progress").style.opacity = "1";
-        // And disable the start button
-        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
-    });
-
-    // Hide the total progress bar when nothing's uploading anymore
-    myDropzone.on("queuecomplete", function (progress) {
-        document.querySelector("#total-progress").style.opacity = "0";
-    });
 </script>
