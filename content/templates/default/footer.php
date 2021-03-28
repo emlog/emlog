00e03f3b3bd13cd23a6507f94c6d9f8939678231
kohunglee <33373536+kohunglee@users.noreply.github.com>
2021-03-28 15:23:52 +0800

    Update footer.php

--- content/templates/default/footer.php
+++ content/templates/default/footer.php
@@ -7,8 +7,8 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 
 <footer class="py-5">
     <div class="container">
-        <p class="m-0 text-center">Powered by <a href="http://www.emlog.net">emlog</a><br>
-            <a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a> <?php echo $footer_info; ?>
+        <p class="m-0 text-center">Powered by <a href="http://www.emlog.net">emlog pro</a><br>
+            <a href="https://beian.miit.gov.cn/" target="_blank"><?php echo $icp; ?></a> <?php echo $footer_info; ?>
             <?php doAction('index_footer'); ?>
         </p>
     </div>
