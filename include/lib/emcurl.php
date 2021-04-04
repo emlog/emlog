2a7e96381c6723ac7fde494ede4306d16327c87a
snow <>
2021-04-04 18:45:15 +0800

    feat:code style format
    Signed-off-by: snow <>

---
+++ include/lib/emcurl.php
@@ -0,0 +1,91 @@
+<?php
+
+class EmCurl {
+	protected $_useragent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1';
+	protected $_url;
+	protected $_followlocation = false;
+	protected $_timeout = 3;// second
+	protected $_maxRedirects = 0;
+	protected $_post;
+	protected $_postFields;
+	protected $_referer = "http://www.google.com";
+
+	protected $_respone;
+	protected $_includeHeader;
+	protected $_noBody;
+	protected $_status;
+	public $authentication = false;
+	public $auth_name = '';
+	public $auth_pass = '';
+
+	public function useAuth($use) {
+		$this->authentication = false;
+		if ($use == true) $this->authentication = true;
+	}
+
+	public function setName($name) {
+		$this->auth_name = $name;
+	}
+
+	public function setPass($pass) {
+		$this->auth_pass = $pass;
+	}
+
+	public function __construct($timeOut = 2, $includeHeader = false, $noBody = false) {
+		$this->_timeout = $timeOut;
+		$this->_noBody = $noBody;
+		$this->_includeHeader = $includeHeader;
+	}
+
+	public function setPost($postFields) {
+		$this->_post = true;
+		$this->_postFields = $postFields;
+	}
+
+	public function request($url = 'nul') {
+		if ($url !== 'nul') {
+			$this->_url = $url;
+		}
+
+		$s = curl_init();
+
+		curl_setopt($s, CURLOPT_URL, $this->_url);
+		curl_setopt($s, CURLOPT_HTTPHEADER, array('Expect:'));
+		curl_setopt($s, CURLOPT_TIMEOUT, $this->_timeout);
+		curl_setopt($s, CURLOPT_MAXREDIRS, $this->_maxRedirects);
+		curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($s, CURLOPT_FOLLOWLOCATION, $this->_followlocation);
+
+		if ($this->authentication) {
+			curl_setopt($s, CURLOPT_USERPWD, $this->auth_name . ':' . $this->auth_pass);
+		}
+
+		if ($this->_post) {
+			curl_setopt($s, CURLOPT_POST, true);
+			curl_setopt($s, CURLOPT_POSTFIELDS, $this->_postFields);
+		}
+
+		if ($this->_includeHeader) {
+			curl_setopt($s, CURLOPT_HEADER, true);
+		}
+
+		if ($this->_noBody) {
+			curl_setopt($s, CURLOPT_NOBODY, true);
+		}
+
+		curl_setopt($s, CURLOPT_USERAGENT, $this->_useragent);
+		curl_setopt($s, CURLOPT_REFERER, $this->_referer);
+
+		$this->_respone = curl_exec($s);
+		$this->_status = curl_getinfo($s, CURLINFO_HTTP_CODE);
+		curl_close($s);
+	}
+
+	public function getHttpStatus() {
+		return $this->_status;
+	}
+
+	public function getRespone() {
+		return $this->_respone;
+	}
+}
