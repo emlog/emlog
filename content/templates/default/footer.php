b317771b567770c0cfbb3c58954870300f58ad26
kohunglee <33373536+kohunglee@users.noreply.github.com>
2021-04-17 18:07:10 +0800

    Update footer.php
    把日历输出的js放入头部。以避免日历不显示

--- content/templates/default/footer.php
+++ content/templates/default/footer.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * 页面底部信息
  */
@@ -9,7 +9,7 @@ if (!defined('EMLOG_ROOT')) {
 
 <footer class="py-5">
     <div class="container">
-        <p class="m-0 text-center">Powered by <a href="http://www.emlog.net">emlog pro</a><br>
+        <p class="m-0 text-center">Powered by <a href="http://www.emlog.net">Emlog Pro</a><br>
             <a href="https://beian.miit.gov.cn/" target="_blank"><?php echo $icp; ?></a> <?php echo $footer_info; ?>
             <?php doAction('index_footer'); ?>
         </p>
@@ -18,7 +18,5 @@ if (!defined('EMLOG_ROOT')) {
 
 <script src="<?php echo TEMPLATE_URL; ?>js/jquery.min.3.5.1.js?v=<?php echo Option::EMLOG_VERSION; ?>"></script>
 <script src="<?php echo TEMPLATE_URL; ?>js/bootstrap.min.js" type="text/javascript"></script>
-<script src="<?php echo TEMPLATE_URL; ?>js/common_tpl.js" type="text/javascript"></script>
-
 </body>
 </html>
