e43cf47059ee36e521743b6ed067e26db6e6bfa8
snow <>
2021-04-24 19:23:47 +0800

    feat:for store
    Signed-off-by: snow <>

--- admin/views/store.php
+++ admin/views/store.php
@@ -15,19 +15,19 @@
     <div class="alert alert-danger">安装失败</div><?php endif; ?>
 <?php if (isset($_GET['error_zip'])): ?>
     <div class="alert alert-danger">安装失败</div><?php endif; ?>
-
+<?php if (isset($_GET['error_source'])): ?>
+    <div class="alert alert-danger">安装失败，不是有效的扩展安装包</div><?php endif; ?>
 
 <?php if (!empty($templates) || !empty($plugins)): ?>
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">扩展商店</h1>
     </div>
-
     <div class="card-columns">
 		<?php foreach ($templates as $k => $v):
 			$icon = $v['icon'] ?: "./views/images/theme.png";
-            ?>
-            <div class="card">
-                <img class="card-img-top" src="<?php echo $icon; ?>" style="height: 150px;object-fit: cover;"/>
+			?>
+            <div class="card" style="min-height: 340px;">
+                <img class="card-img-top" src="<?php echo $icon; ?>"/>
                 <div class="card-body">
                     <p class="card-text"><?php echo $v['name']; ?></p>
                     <p class="card-text text-muted small">
@@ -36,35 +36,33 @@
                         <span class="badge badge-pill badge-warning">模板</span>
                     </p>
                     <p class="card-text text-right">
-                        <a href="./store.php?action=install&source=<?php echo $v['source']; ?>&type=tpl" class="btn btn-success btn-sm">下载安装</a>
+                        <a href="./store.php?action=install&source=<?php echo urlencode($v['source']); ?>&type=tpl" class="btn btn-success btn-sm">下载安装</a>
                     </p>
                 </div>
             </div>
 		<?php endforeach; ?>
     </div>
-
     <div class="card-columns">
 		<?php foreach ($plugins as $k => $v):
 			$icon = $v['icon'] ?: "./views/images/plugin.png";
-            ?>
+			?>
             <div class="card">
                 <img class="card-img-top" src="<?php echo $icon; ?>" style="height: 150px;object-fit: cover;"/>
                 <div class="card-body">
                     <p class="card-text"><?php echo $v['name']; ?></p>
                     <p class="card-text text-muted small">
-                        <?php echo $v['info']; ?><br>
+						<?php echo $v['info']; ?><br>
                         更新时间：<?php echo $v['update_time']; ?><br>
                         <span class="badge badge-pill badge-primary">插件</span>
                     </p>
                     <p class="card-text text-right">
-                        <a href="./store.php?action=install&source=<?php echo $v['source']; ?>&type=plugin" class="btn btn-success btn-sm">下载安装</a>
+                        <a href="./store.php?action=install&source=<?php echo urlencode($v['source']); ?>&type=plugin" class="btn btn-success btn-sm">下载安装</a>
                     </p>
                 </div>
             </div>
 		<?php endforeach; ?>
     </div>
 <?php endif; ?>
-
 <script>
     $("#menu_store").addClass('active');
     setTimeout(hideActived, 3600);
