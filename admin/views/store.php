1763da90de57bf70ecff260349270147b32b7e31
snow <emloog@gmail.com>
2021-04-24 15:27:57 +0800

    feat:for store
    Signed-off-by: snow <>

--- admin/views/store.php
+++ admin/views/store.php
@@ -1,46 +1,69 @@
 <?php if (!defined('EMLOG_ROOT')) {
 	exit('error!');
 } ?>
-<?php if (isset($_GET['active_del'])): ?>
+<?php if (isset($_GET['active'])): ?>
     <div class="alert alert-success">安装成功</div><?php endif; ?>
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">扩展商店</h1>
-    <a href="#" class="d-none d-sm-inline-block btn btn-success shadow-sm" data-toggle="modal" data-target="#exampleModal"><i class="icofont-plus"></i> 上传图片/文件</a>
-</div>
+<?php if (isset($_GET['error'])): ?>
+    <div class="alert alert-danger">商店暂不可用，可能是网络问题</div><?php endif; ?>
+<?php if (isset($_GET['error_unreg'])): ?>
+    <div class="alert alert-danger">您的emlog pro尚未注册，请先完成注册</div><?php endif; ?>
+<?php if (isset($_GET['error_param'])): ?>
+    <div class="alert alert-danger">安装失败</div><?php endif; ?>
+<?php if (isset($_GET['error_down'])): ?>
+    <div class="alert alert-danger">安装失败</div><?php endif; ?>
+<?php if (isset($_GET['error_dir'])): ?>
+    <div class="alert alert-danger">安装失败</div><?php endif; ?>
+<?php if (isset($_GET['error_zip'])): ?>
+    <div class="alert alert-danger">安装失败</div><?php endif; ?>
 
-<div class="card-columns">
-	<?php foreach ($attach as $key => $value):
-		$extension = strtolower(substr(strrchr($value['filepath'], "."), 1));
-		$atturl = BLOG_URL . substr($value['filepath'], 3);
-		$name =  $value['filename'];
-		if (in_array($extension, array('gif', 'jpg', 'jpeg', 'png'))) {
-			$imgpath = $value['filepath'];
-			if (isset($value['thum_filepath'])) {
-				$imgpath = BLOG_URL . substr($value['thum_filepath'], 3);
-			}
-		} else {
-			$imgpath = "./views/images/fnone.png";
-		}
-		?>
-        <div class="card">
-            <a href="<?php echo $atturl; ?>" target="_blank" title="<?php echo $name; ?>">
-                <img class="card-img-top" src="<?php echo $imgpath; ?>" style="height: 150px;object-fit: cover;"/>
-            </a>
-            <div class="card-body">
-                <p class="card-text text-muted small">
-                    时间：<?php echo $value['addtime']; ?><br>
-                    大小：<?php echo $value['attsize']; ?>
-					<?php if ($value['width'] && $value['height']): ?>
-                        尺寸：<?php echo $value['width'] ?>x<?php echo $value['height'] ?>
-					<?php endif; ?>
-                </p>
-                <p class="card-text">
-                    <a href="javascript: em_confirm(<?php echo $value['aid']; ?>, 'media', '<?php echo LoginAuth::genToken(); ?>');" class="badge badge-danger">删除</a>
-                </p>
+
+<?php if (!empty($templates) || !empty($plugins)): ?>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">扩展商店</h1>
+    </div>
+
+    <div class="card-columns">
+		<?php foreach ($templates as $k => $v):
+			$icon = $v['icon'] ?: "./views/images/theme.png";
+            ?>
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $icon; ?>" style="height: 150px;object-fit: cover;"/>
+                <div class="card-body">
+                    <p class="card-text"><?php echo $v['name']; ?></p>
+                    <p class="card-text text-muted small">
+                        <span class="small"><?php echo $v['info']; ?></span><br>
+                        更新时间：<?php echo $v['update_time']; ?><br>
+                        <span class="badge badge-pill badge-warning">模板</span>
+                    </p>
+                    <p class="card-text text-right">
+                        <a href="./store.php?action=install&source=<?php echo $v['source']; ?>&type=tpl" class="btn btn-success btn-sm">下载安装</a>
+                    </p>
+                </div>
             </div>
-        </div>
-	<?php endforeach; ?>
-</div>
+		<?php endforeach; ?>
+    </div>
+
+    <div class="card-columns">
+		<?php foreach ($plugins as $k => $v):
+			$icon = $v['icon'] ?: "./views/images/plugin.png";
+            ?>
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $icon; ?>" style="height: 150px;object-fit: cover;"/>
+                <div class="card-body">
+                    <p class="card-text"><?php echo $v['name']; ?></p>
+                    <p class="card-text text-muted small">
+                        <?php echo $v['info']; ?><br>
+                        更新时间：<?php echo $v['update_time']; ?><br>
+                        <span class="badge badge-pill badge-primary">插件</span>
+                    </p>
+                    <p class="card-text text-right">
+                        <a href="./store.php?action=install&source=<?php echo $v['source']; ?>&type=plugin" class="btn btn-success btn-sm">下载安装</a>
+                    </p>
+                </div>
+            </div>
+		<?php endforeach; ?>
+    </div>
+<?php endif; ?>
 
 <script>
     $("#menu_store").addClass('active');
