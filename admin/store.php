4f3fcb8dfec50cff93e6014a70222832acc27141
snow <>
2021-04-24 15:27:57 +0800

    feat:for store
    Signed-off-by: snow <>

--- admin/store.php
+++ admin/store.php
@@ -11,26 +11,49 @@
 
 require_once 'globals.php';
 
-$Link_Model = new Link_Model();
-
 if (empty($action)) {
-	$links = $Link_Model->getLinks();
+	$emcurl = new EmCurl();
+	$emcurl->setPost(['emkey' => Option::get('emkey'), 'ver'   => Option::EMLOG_VERSION,]);
+	$emcurl->request(OFFICIAL_SERVICE_HOST . 'service/store');
+	$retStatus = $emcurl->getHttpStatus();
+	if ($retStatus !== 200) {
+		emDirect("./store.php?action=error&error=1");
+	}
+	$respone = $emcurl->getRespone();
+	$ret = json_decode($respone, 1);
+	if (empty($ret)) {
+		emDirect("./store.php?action=error&error=1");
+	}
+	if ($ret['code'] === 1001) {
+		emDirect("./store.php?action=error&error_unreg=1");
+	}
+
+	$templates = $ret['data']['templates'] ?? [];
+	$plugins = $ret['data']['plugins'] ?? [];
+
 	include View::getView('header');
 	require_once(View::getView('store'));
 	include View::getView('footer');
 	View::output();
 }
 
-if ($action === 'addon') {
+if ($action === 'error') {
+	include View::getView('header');
+	require_once(View::getView('store'));
+	include View::getView('footer');
+	View::output();
+}
+
+if ($action === 'install') {
 	$source = isset($_GET['source']) ? trim($_GET['source']) : '';
 	$source_type = isset($_GET['type']) ? trim($_GET['type']) : '';
 	if (empty($source)) {
-		exit('error');
+		emDirect("./store.php?error_param=1");
 	}
 
 	$temp_file = emFecthFile(OFFICIAL_SERVICE_HOST . $source);
 	if (!$temp_file) {
-		exit('error_down');
+		emDirect("./store.php?error_down=1");
 	}
 
 	$unzip_path = $source_type == 'tpl' ? '../content/templates/' : '../content/plugins/';
@@ -38,11 +61,11 @@ if ($action === 'addon') {
 	@unlink($temp_file);
 	switch ($ret) {
 		case 0:
-			exit('succ');
+			emDirect("./store.php?active=1");
 		case 1:
 		case 2:
-			exit('error_dir');
+			emDirect("./store.php?error_dir=1");
 		case 3:
-			exit('error_zip');
+			emDirect("./store.php?error_zip=1");
 	}
 }
