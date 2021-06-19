302123ae80bdc45d6857ab26324b2f7b09c85745
snow <>
2021-06-19 23:50:08 +0800

    feat:for upgrade
    Signed-off-by: snow <>

--- upgrade.sql
+++ upgrade.sql
@@ -1,11 +1,10 @@
 # emlog pro database upgrade sql
 
 # 修改已有字段
-ALTER TABLE {db_prefix}blog CHANGE COLUMN comnum comnum int(10) unsigned NOT NULL default '0';
+# -- eg: ALTER TABLE {db_prefix}blog CHANGE COLUMN comnum comnum int(10) unsigned NOT NULL default '0';
 
 # 插入新的配置记录
-INSERT INTO {db_prefix}options (option_name, option_value) VALUES ('att_img2maxw','420');
+# -- eg: INSERT INTO {db_prefix}options (option_name, option_value) VALUES ('att_imgmaxw','420');
 
 # 添加新的字段
-ALTER TABLE {db_prefix}blog ADD COLUMN sortop enum('n','y') NOT NULL default 'n' AFTER top;
-ALTER TABLE {db_prefix}blog ADD COLUMN sortop2 enum('n','y') NOT NULL default 'n' AFTER top;
+# -- eg: ALTER TABLE {db_prefix}blog ADD COLUMN sortop enum('n','y') NOT NULL default 'n' AFTER top;
