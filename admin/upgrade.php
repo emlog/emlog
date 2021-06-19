e0c83fb8e7b0c7bcd050c93c19889ef592e243a5
snow <>
2021-06-19 22:30:19 +0800

    feat:for upgrade
    Signed-off-by: snow <>

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -62,14 +62,12 @@ if ($action === 'update' && ROLE === ROLE_ADMIN) {
 		exit('succ');
 	}
 	$DB = Database::getInstance();
-	$setchar = "ALTER DATABASE `" . DB_NAME . "` DEFAULT CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;";
 	$temp_file = emFecthFile($upsql);
 	if (!$temp_file) {
 		exit('error_down');
 	}
 	$sql = file($temp_file);
 	@unlink($temp_file);
-	array_unshift($sql, $setchar);
 	$query = '';
 	foreach ($sql as $value) {
 		if (!$value || $value[0] == '#') {
