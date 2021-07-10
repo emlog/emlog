116f9579d565e9c5a5fbb3bb072b68025e37a4c6
kohunglee <33373536+kohunglee@users.noreply.github.com>
2021-07-10 12:23:20 +0800

    Update footer.php
    更新缓存

--- admin/views/footer.php
+++ admin/views/footer.php
@@ -13,6 +13,6 @@
 </div>
 </div>
 <?php doAction('adm_footer'); ?>
-<script src="./views/js/sb-admin-2.min.js"></script>
+<script src="./views/js/sb-admin-2.min.js?d=7.10.2021"></script>
 </body>
 </html>
