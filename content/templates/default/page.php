52ffd5d6c0cf71aa0ffed2d9ac016eba02ed92c4
snow <>
2021-08-25 23:53:43 +0800

    fix: page markdown
    Signed-off-by: snow <>

--- content/templates/default/page.php
+++ content/templates/default/page.php
@@ -6,7 +6,7 @@ if (!defined('EMLOG_ROOT')) {
 	exit('error!');
 }
 ?>
-<style type="text/css">
+<style>
     .page {
         background-color: #ffffff;
         border-radius: 10px;
@@ -16,13 +16,12 @@ if (!defined('EMLOG_ROOT')) {
     .com_submit_p {
         font-size: 15px
     }
-
 </style>
 <div class="container">
     <div class="row">
         <div class="col-md-8 page">
             <h2><?php echo $log_title; ?></h2>
-			<?php echo $log_content; ?>
+            <div class="markdown"><?php echo $log_content; ?></div>
 			<?php blog_comments($comments); ?>
 			<?php blog_comments_post($logid, $ckname, $ckmail, $ckurl, $verifyCode, $allow_remark); ?>
         </div>
@@ -32,15 +31,14 @@ if (!defined('EMLOG_ROOT')) {
     </div>
 </div>
 <script>
-function change_com_radius(){
-    if(!document.getElementById("com_info"))
-    {
-        var comment = document.getElementById("comment");
-        comment.style.height = "140px";
-        comment.style.setProperty('border-radius', '10px');
+    function change_com_radius() {
+        if (!document.getElementById("com_info")) {
+            var comment = document.getElementById("comment");
+            comment.style.height = "140px";
+            comment.style.setProperty('border-radius', '10px');
+        }
     }
-}
-change_com_radius();
+    change_com_radius();
 </script>
 <?php
 include View::getView('footer');
