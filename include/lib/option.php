6b679a4e2645bf88da77aa6eb0db888736ca2c95
snow <>
2021-10-08 08:53:04 +0800

    release 1.0.7
    Signed-off-by: snow <>

--- include/lib/option.php
+++ include/lib/option.php
@@ -6,8 +6,8 @@
 
 class Option {
 
-	const EMLOG_VERSION = 'pro v1.0.6';              //版本编号
-	const EMLOG_VERSION_TIMESTAMP = 1632128457;      //版本时间戳
+	const EMLOG_VERSION = 'pro v1.0.7';              //版本编号
+	const EMLOG_VERSION_TIMESTAMP = 1633654283;      //版本时间戳
 	const ICON_MAX_W = 160;                          //头像缩略图最大宽
 	const ICON_MAX_H = 160;                          //头像缩略图最大高
 	const UPLOADFILE_PATH = '../content/uploadfile/';//上传路径
