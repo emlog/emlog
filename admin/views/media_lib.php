95b1cb6a12107deff5a38d2df28ef263eff619d6
snow <emloog@gmail.com>
2021-11-18 11:22:49 +0800

    fix:card list
    Signed-off-by: snow <>

--- admin/views/media_lib.php
+++ admin/views/media_lib.php
@@ -3,7 +3,7 @@
 } ?>
 
 <?php foreach ($medias as $key => $value):
-    $media_path = $value['filepath'];
+	$media_path = $value['filepath'];
 	$media_url = getFileUrl($media_path);
 	$media_name = $value['filename'];
 	if (isImage($value['filepath'])) {
@@ -12,24 +12,26 @@
 		$media_icon = "./views/images/fnone.png";
 	}
 	?>
-    <div class="card" style="min-height: 280px;">
-        <a href="<?php echo $media_url; ?>" target="_blank""><img class="card-img-top" src="<?php echo $media_icon; ?>"/></a>
-        <div class="card-body">
-            <p class="card-text text-muted small">
-				<?php echo $media_name; ?><br>
-                上传时间：<?php echo $value['addtime']; ?><br>
-                文件大小：<?php echo $value['attsize']; ?>，
-            </p>
-            <p class="card-text d-flex justify-content-between">
-				<?php if (isImage($value['filepath'])): ?>
-                    <a href="javascript:insert_media_img('<?php echo $media_url; ?>', '<?php echo $media_icon; ?>')" title="插入到文章"><i class="icofont-plus"></i></a>
-                    <a href="javascript:insert_conver('<?php echo $media_path; ?>')" title="设为封面"><i class="icofont-image"></i></a>
-				<?php elseif (isVideo($value['filepath'])): ?>
-                    <a href="javascript:insert_media_video('<?php echo $media_url; ?>')"><i class="icofont-plus"></i></a>
-				<?php else: ?>
-                    <a href="javascript:insert_media('<?php echo $media_url; ?>', '<?php echo $media_name; ?>')"><i class="icofont-plus"></i></a>
-				<?php endif; ?>
-            </p>
+    <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+            <a href="<?php echo $media_url; ?>" target="_blank""><img class="card-img-top" src="<?php echo $media_icon; ?>"/></a>
+            <div class="card-body">
+                <p class="card-text text-muted small">
+					<?php echo $media_name; ?><br>
+                    上传时间：<?php echo $value['addtime']; ?><br>
+                    文件大小：<?php echo $value['attsize']; ?>，
+                </p>
+                <p class="card-text d-flex justify-content-between">
+					<?php if (isImage($value['filepath'])): ?>
+                        <a href="javascript:insert_media_img('<?php echo $media_url; ?>', '<?php echo $media_icon; ?>')" title="插入到文章"><i class="icofont-plus"></i></a>
+                        <a href="javascript:insert_conver('<?php echo $media_path; ?>')" title="设为封面"><i class="icofont-image"></i></a>
+					<?php elseif (isVideo($value['filepath'])): ?>
+                        <a href="javascript:insert_media_video('<?php echo $media_url; ?>')"><i class="icofont-plus"></i></a>
+					<?php else: ?>
+                        <a href="javascript:insert_media('<?php echo $media_url; ?>', '<?php echo $media_name; ?>')"><i class="icofont-plus"></i></a>
+					<?php endif; ?>
+                </p>
+            </div>
         </div>
     </div>
 <?php endforeach; ?>
