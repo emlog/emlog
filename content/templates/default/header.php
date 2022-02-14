d6acff43c4322943e51a59fe6b0ad4b19a26e443
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-02-14 15:25:39 +0800

    feat:添加 base 标签，目前主要作用是供 js 获取有效的 blog_url 值
    不使用它的主要功能，因为这个是不常用标签，会造成参考人员的困惑。不过一方面有了它也会有利于搜索引擎优化。

--- content/templates/default/header.php
+++ content/templates/default/header.php
@@ -20,6 +20,7 @@ require_once View::getView('module');
     <meta name="keywords" content="<?= $site_key ?>"/>
     <meta name="description" content="<?= $site_description ?>"/>
     <meta name="generator" content="emlog"/>
+    <base href="<?= BLOG_URL ?>" />
     <link rel="shortcut icon" href="<?= BLOG_URL ?>favicon.ico"/>
     <link rel="bookmark" href="<?= BLOG_URL ?>favicon.ico" type="image/x-icon" 　/>
     <link rel="alternate" title="RSS" href="<?= BLOG_URL ?>rss.php" type="application/rss+xml"/>
