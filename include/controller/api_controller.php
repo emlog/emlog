3d11ded1df87e02a8472fed21bffc5e7d91fc692
snow <emloog@gmail.com>
2022-06-04 19:30:32 +0800

    feat:api add article list and detail

--- include/controller/api_controller.php
+++ include/controller/api_controller.php
@@ -73,7 +73,76 @@ class Api_Controller {
 
 	}
 
-	private function checkApiKey($req_sign, $req_time) {
+	private function article_list() {
+		$page = isset($_GET['page']) ? (int)trim($_GET['page']) : 1;
+		$count = isset($_GET['count']) ? (int)trim($_GET['count']) : Option::get('index_lognum');
+		$sort_id = isset($_GET['sort_id']) ? (int)trim($_GET['sort_id']) : 0;
+
+		$sub = '';
+		if ($sort_id) {
+			$sub .= ' and sortid = ' . $sort_id;
+		}
+
+		$r = $this->Log_Model->getLogsForHome($sub . " ORDER BY top DESC ,date DESC", $page, $count);
+		$sort_cache = $this->Cache->readCache('sort');
+		$author_cache = $this->Cache->readCache('user');
+		$articles = [];
+		foreach ($r as $value) {
+			$articles[] = [
+				'id'          => (int)$value['gid'],
+				'title'       => $value['title'],
+				'cover'       => $value['log_cover'],
+				'url'         => $value['log_url'],
+				'description' => $value['log_description'],
+				'date'        => date('Y-m-d H:i:s', $value['date']),
+				'author_id'   => (int)$value['author'],
+				'author_name' => $author_cache[$value['author']]['name'] ?? '',
+				'sort_id'     => (int)$value['sortid'],
+				'sort_name'   => $sort_cache[$value['sortid']]['sortname'] ?? '',
+				'views'       => (int)$value['views'],
+				'comnum'      => (int)$value['comnum'],
+				'top'         => $value['top'],
+				'sortop'      => $value['sortop'],
+			];
+		}
+
+		output::ok([
+			'articles' => $articles,
+		]);
+	}
+
+	private function article_detail() {
+		$id = isset($_GET['id']) ? (int)trim($_GET['id']) : 0;
+
+		$r = $this->Log_Model->getOneLogForHome($id);
+		$sort_cache = $this->Cache->readCache('sort');
+		$author_cache = $this->Cache->readCache('user');
+		$article = '';
+		if ($r) {
+			$article = [
+				'title'       => $r['log_title'],
+				'date'        => date('Y-m-d H:i:s', $r['date']),
+				'id'          => (int)$r['logid'],
+				'sort_id'     => (int)$r['sortid'],
+				'sort_name'   => $sort_cache[$r['sortid']]['sortname'] ?? '',
+				'type'        => $r['type'],
+				'author_id'   => $r['author'],
+				'author_name' => $author_cache[$r['author']]['name'] ?? '',
+				'content'     => $r['log_content'],
+				'cover'       => $r['log_cover'],
+				'views'       => (int)$r['views'],
+				'comnum'      => (int)$r['comnum'],
+				'top'         => $r['top'],
+				'sortop'      => $r['sortop'],
+			];
+		}
+		output::ok([
+			'article' => $article,
+		]);
+	}
+
+	private
+	function checkApiKey($req_sign, $req_time) {
 		$apikey = Option::get('apikey');
 		$sign = md5($req_time . $apikey);
 
