c7677380c5937185886d8cf1e8cea52e11e81956
snow <emloog@gmail.com>
2022-10-03 16:12:04 +0800

    feat:解决图片跳转链接的问题

--- include/lib/option.php
+++ include/lib/option.php
@@ -7,8 +7,8 @@
 
 class Option {
 
-	const EMLOG_VERSION = 'pro 1.6.0';               //版本编号
-	const EMLOG_VERSION_TIMESTAMP = 1660446301;      //版本时间戳
+	const EMLOG_VERSION = 'pro 1.7.0';               //版本编号
+	const EMLOG_VERSION_TIMESTAMP = 1664784625;      //版本时间戳
 	const ICON_MAX_W = 160;                          //头像缩略图最大宽
 	const ICON_MAX_H = 160;                          //头像缩略图最大高
 	const UPLOADFILE_PATH = '../content/uploadfile/';//上传路径
