86d334c9dafa67d9b341a8b4baef8aaaae658891
snow <emloog@gmail.com>
2022-11-02 22:38:38 +0800

    feat:email notice

--- include/lib/function.base.php
+++ include/lib/function.base.php
@@ -38,12 +38,14 @@ function htmlClean($content, $nl2br = true) {
 /**
  * 获取用户ip地址
  */
-function getIp() {
-	$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-	if (!filter_var($ip, FILTER_VALIDATE_IP)) {
-		$ip = '';
+if (!function_exists('getIp')) {
+	function getIp() {
+		$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+		if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+			$ip = '';
+		}
+		return $ip;
 	}
-	return $ip;
 }
 
 /**
@@ -655,10 +657,12 @@ function chImageSize($img, $max_w, $max_h) {
 /**
  * 获取Gravatar头像
  */
-function getGravatar($email, $s = 40) {
-	$hash = md5($email);
+if (!function_exists('getGravatar')) {
+	function getGravatar($email, $s = 40) {
+		$hash = md5($email);
 //	return "//gravatar.loli.net/avatar/$hash?s=$s";
-	return "//sdn.geekzu.org/avatar/$hash?s=$s";
+		return "//sdn.geekzu.org/avatar/$hash?s=$s";
+	}
 }
 
 /**
@@ -1156,38 +1160,43 @@ if (!function_exists('split')) {
 
 em_v();
 
-function get_os($user_agent) {
-	if (false !== stripos($user_agent, "win")) {
-		$os = 'Windows';
-	} else if (false !== stripos($user_agent, "mac")) {
-		$os = 'MAC';
-	} else if (false !== stripos($user_agent, "linux")) {
-		$os = 'Linux';
-	} else if (false !== stripos($user_agent, "unix")) {
-		$os = 'Unix';
-	} else if (false !== stripos($user_agent, "bsd")) {
-		$os = 'BSD';
-	} else {
-		$os = 'unknown';
+if (!function_exists('get_os')) {
+	function get_os($user_agent) {
+		if (false !== stripos($user_agent, "win")) {
+			$os = 'Windows';
+		} else if (false !== stripos($user_agent, "mac")) {
+			$os = 'MAC';
+		} else if (false !== stripos($user_agent, "linux")) {
+			$os = 'Linux';
+		} else if (false !== stripos($user_agent, "unix")) {
+			$os = 'Unix';
+		} else if (false !== stripos($user_agent, "bsd")) {
+			$os = 'BSD';
+		} else {
+			$os = 'unknown';
+		}
+		return $os;
 	}
-	return $os;
 }
 
-function get_browse($user_agent) {
-	if (false !== stripos($user_agent, "MSIE")) {
-		$br = 'MSIE';
-	} else if (false !== stripos($user_agent, "Edg")) {
-		$br = 'Edge';
-	} else if (false !== stripos($user_agent, "Firefox")) {
-		$br = 'Firefox';
-	} else if (false !== stripos($user_agent, "Chrome")) {
-		$br = 'Chrome';
-	} else if (false !== stripos($user_agent, "Safari")) {
-		$br = 'Safari';
-	} else if (false !== stripos($user_agent, "Opera")) {
-		$br = 'Opera';
-	} else {
-		$br = 'unknown';
+if (!function_exists('get_browse')) {
+	function get_browse($user_agent) {
+		if (false !== stripos($user_agent, "MSIE")) {
+			$br = 'MSIE';
+		} else if (false !== stripos($user_agent, "Edg")) {
+			$br = 'Edge';
+		} else if (false !== stripos($user_agent, "Firefox")) {
+			$br = 'Firefox';
+		} else if (false !== stripos($user_agent, "Chrome")) {
+			$br = 'Chrome';
+		} else if (false !== stripos($user_agent, "Safari")) {
+			$br = 'Safari';
+		} else if (false !== stripos($user_agent, "Opera")) {
+			$br = 'Opera';
+		} else {
+			$br = 'unknown';
+		}
+		return $br;
 	}
-	return $br;
 }
+
