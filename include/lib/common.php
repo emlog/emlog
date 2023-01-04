18ca0df9cf68c03273af08e47e13d236440d639b
snow <emloog@gmail.com>
2023-01-04 17:07:17 +0800

    feat:use MBSTRING ext php5.6

--- include/lib/common.php
+++ include/lib/common.php
@@ -127,36 +127,8 @@ function checkMail($email) {
  * @param int $length 截取长度
  */
 function subString($strings, $start, $length) {
-	if (function_exists('mb_substr') && function_exists('mb_strlen')) {
-		$sub_str = mb_substr($strings, $start, $length, 'utf8');
-		return mb_strlen($sub_str, 'utf8') < mb_strlen($strings, 'utf8') ? $sub_str . '...' : $sub_str;
-	}
-	$str = substr($strings, $start, $length);
-	$char = 0;
-	for ($i = 0, $iMax = strlen($str); $i < $iMax; $i++) {
-		if (ord($str[$i]) >= 128)
-			$char++;
-	}
-	$str2 = substr($strings, $start, $length + 1);
-	$str3 = substr($strings, $start, $length + 2);
-	if ($char % 3 == 1) {
-		if ($length <= strlen($strings)) {
-			$str3 = $str3 .= '...';
-		}
-		return $str3;
-	}
-	if ($char % 3 == 2) {
-		if ($length <= strlen($strings)) {
-			$str2 = $str2 .= '...';
-		}
-		return $str2;
-	}
-	if ($char % 3 == 0) {
-		if ($length <= strlen($strings)) {
-			$str = $str .= '...';
-		}
-		return $str;
-	}
+	$sub_str = mb_substr($strings, $start, $length, 'utf8');
+	return mb_strlen($sub_str, 'utf8') < mb_strlen($strings, 'utf8') ? $sub_str . '...' : $sub_str;
 }
 
 /**
