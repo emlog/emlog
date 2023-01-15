11d7ed518d1d6ae10ab6144585959730e893d870
kohunglee <33373536+kohunglee@users.noreply.github.com>
2023-01-15 22:41:11 +0800

    fix:Prevent Mobile phone click the toolbar when touchmoving

--- admin/editor.md/editormd.js
+++ admin/editor.md/editormd.js
@@ -1206,7 +1206,7 @@
             }, false);
 
             toolbarIcons.bind(editormd.mouseOrTouch("click", "touchend"), function (event) {
-                if (isTouchMove === true) return  /* 如果用户正在滑动屏幕，则不触发事件 */
+                if (isTouchMove === true) return  // 如果用户正在滑动屏幕，则不触发事件
 
                 var icon = $(this).children(".fa");
                 var name = icon.attr("name");
