77890dc074df137d4b4d20d2c9d602a669649db7
snow <emloog@gmail.com>
2023-02-28 23:38:55 +0800

    feat:Optimize tag pag ,add tag search

--- admin/views/tag.php
+++ admin/views/tag.php
@@ -32,18 +32,18 @@
         <div class="card-body">
             <div>
 				<?php if ($tags): ?>
-					<?php foreach ($tags as $key => $v):
+					<?php foreach ($tags
+
+								   as $key => $v):
 						$count = count(explode(',', $v['gid']));
 						$count_style = $count > 0 ? 'text-muted' : 'text-danger';
 						?>
-                        <a href="#" class="badge badge-light m-4 p-1" data-toggle="modal" data-target="#editModal" data-tid="<?= $v['tid'] ?>"
-                           data-tagname="<?= $v['tagname'] ?>">
-                            <h5><?= $v['tagname'] ?></h5>
-                            <small class="<?= $count_style ?>">
-                                文章<?= $count ?>篇
-                            </small>
+                        <div class="badge badge-light m-3 p-2">
+                            <h5><a href="#" data-toggle="modal" data-target="#editModal" data-tid="<?= $v['tid'] ?>"
+                                   data-tagname="<?= $v['tagname'] ?>"><?= $v['tagname'] ?></a></h5>
+                            <small class="<?= $count_style ?>">（文章：<?= $count ?>）</small>
                             <input type="checkbox" name="tids[]" value="<?= $v['tid'] ?>" class="tids align-top"/>
-                        </a>
+                        </div>
 					<?php endforeach ?>
 				<?php else: ?>
                     <p class="m-3">还没有标签，写文章的时候可以给文章打标签</p>
