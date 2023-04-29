bc97a12beb74ea73101d0905554862b9e40b1d59
kohunglee <33373536+kohunglee@users.noreply.github.com>
2023-04-29 23:30:46 +0800

    fix:Modified some RSS validation errors

--- rss.php
+++ rss.php
@@ -12,9 +12,13 @@ $Log_Model = new Log_Model();
 $articles = $Log_Model->getLogsForRss(Option::get('rss_output_num'));
 
 echo '<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0">
+<rss version="2.0"
+xmlns:dc="http://purl.org/dc/elements/1.1/"
+xmlns:atom="http://www.w3.org/2005/Atom"
+>
 <channel>
 <title><![CDATA[' . Option::get('blogname') . ']]></title> 
+<atom:link href="'.Option::get('blogurl').'rss.php" rel="self" type="application/rss+xml" />
 <description><![CDATA[' . Option::get('bloginfo') . ']]></description>
 <link>' . BLOG_URL . '</link>
 <language>zh-cn</language>
@@ -33,7 +37,7 @@ if (!empty($articles)) {
     <link>$link</link>
     <description><![CDATA[{$abstract}]]></description>
     <pubDate>$pubdate</pubDate>
-    <author>$author</author>
+    <dc:creator>$author</dc:creator>
     <guid>$link</guid>
 </item>
 END;
