96a20f4cde7c993937c71a20aa978cf5ecd4a6da
snow <emloog@gmail.com>
2023-04-29 13:29:27 +0800

    fix:tag not use do not show 404 page

--- include/controller/tag_controller.php
+++ include/controller/tag_controller.php
@@ -15,26 +15,26 @@ class Tag_Controller {
         $page = isset($params[4]) && $params[4] == 'page' ? abs((int)$params[5]) : 1;
         $tag = isset($params[1]) && $params[1] == 'tag' ? addslashes(urldecode(trim($params[2]))) : '';
 
-        $pageurl = '';
-
         //page meta
         $site_title = stripslashes($tag) . ' - ' . $site_title;
 
         $Tag_Model = new Tag_Model();
         $blogIdStr = $Tag_Model->getTagByName($tag);
-        if (!$blogIdStr) {
-            show_404_page();
+        $lognum = 0;
+        $total_pages = 0;
+        $logs = [];
+        if ($blogIdStr) {
+            $sqlSegment = "and gid IN ($blogIdStr) order by date desc";
+            $logs = $Log_Model->getLogsForHome($sqlSegment, $page, $index_lognum);
+            $lognum = $Log_Model->getLogNum('n', $sqlSegment);
+            $total_pages = ceil($lognum / $index_lognum);
         }
-        $sqlSegment = "and gid IN ($blogIdStr) order by date desc";
-        $lognum = $Log_Model->getLogNum('n', $sqlSegment);
-        $total_pages = ceil($lognum / $index_lognum);
+
         if ($page > $total_pages) {
             $page = $total_pages;
         }
-        $pageurl .= Url::tag(urlencode($tag), 'page');
 
-        $logs = $Log_Model->getLogsForHome($sqlSegment, $page, $index_lognum);
-        $page_url = pagination($lognum, $index_lognum, $page, $pageurl);
+        $page_url = pagination($lognum, $index_lognum, $page, Url::tag(urlencode($tag), 'page'));
 
         include View::getView('header');
         include View::getView('log_list');
