455e3353e69f318e89c49aabf0d3a3c168d399a7
snow <emloog@gmail.com>
2023-05-02 23:31:07 +0800

    fix:err note link

--- admin/views/index_user.php
+++ admin/views/index_user.php
@@ -45,7 +45,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">笔记</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="./user.php"><?= $note_amount ?></a></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="./twitter.php"><?= $note_amount ?></a></div>
                     </div>
                     <div class="col-auto">
                         <i class="icofont-user fa-2x text-gray-300"></i>
