b1c1e784c5072b0703fca5c6eefd0ec22de0af62
snow <emloog@gmail.com>
2023-06-04 19:25:47 +0800

    release 2.1.8

--- include/lib/option.php
+++ include/lib/option.php
@@ -7,8 +7,8 @@
 
 class Option {
 
-    const EMLOG_VERSION = 'pro 2.1.7';
-    const EMLOG_VERSION_TIMESTAMP = 1685358757;
+    const EMLOG_VERSION = 'pro 2.1.8';
+    const EMLOG_VERSION_TIMESTAMP = 1685877916;
     const UPLOADFILE_PATH = '../content/uploadfile/';
 
     static function get($option) {
