6b8dc16485630fff8eb4a15400832067bc821d09
snow <emloog@gmail.com>
2023-08-09 09:33:04 +0800

    feat:优化页面编辑布局

--- admin/views/page_create.php
+++ admin/views/page_create.php
@@ -4,7 +4,7 @@
 <form action="page.php?action=save" method="post" enctype="multipart/form-data" id="addlog" name="addlog">
     <h1 class="h3 mb-4 text-gray-800"><?= $containertitle ?></h1>
     <div class="row">
-        <div class="col-xl-12">
+        <div class="col-xl-9">
             <div id="post" class="form-group">
                 <div>
                     <input type="text" name="title" id="title" value="<?= $title ?>" class="form-control" placeholder="页面标题"/>
@@ -15,46 +15,49 @@
                 </div>
                 <div id="pagecontent"><textarea><?= $content ?></textarea></div>
             </div>
-
-            <div class="form-group">
-                <label>链接别名：（用于seo设置 <a href="./setting.php?action=seo">&rarr;</a>）</label>
-                <input name="alias" id="alias" class="form-control" value="<?= $alias ?>"/>
-            </div>
-            <div class="form-group">
-                <label>跳转链接：<small class="text-muted">（填写后不展示页面内容，直接跳转该地址）</small></label>
-                <input name="link" id="link" type="url" class="form-control" value="<?= $link ?>" placeholder="https://"/>
-            </div>
-            <div class="form-group">
-                <label>页面模板：</label>
-                <?php if ($customTemplates):
-                    $sortListHtml = '<option value="">默认</option>';
-                    foreach ($customTemplates as $v) {
-                        $select = $v['filename'] == $template ? 'selected="selected"' : '';
-                        $sortListHtml .= '<option value="' . str_replace('.php', '', $v['filename']) . '" ' . $select . '>' . ($v['comment']) . '</option>';
-                    }
-                    ?>
-                    <select id="template" name="template" class="form-control"><?= $sortListHtml; ?></select>
-                    <small class="form-text text-muted">(选择当前模板支持的页面模板，可不选)</small>
-                <?php else: ?>
-                    <input class="form-control" id="template" name="template" value="<?= $template ?>">
-                    <small class="form-text text-muted">(用于自定义页面模板，对应模板目录下xxx.php文件，xxx即为模板名，可不填)</small>
-                <?php endif; ?>
-            </div>
-            <div class="form-group">
-                <input type="checkbox" value="y" name="allow_remark" id="allow_remark" <?= $is_allow_remark ?> />
-                <label for="allow_remark">允许评论</label>
-            </div>
-            <div class="form-group">
-                <input type="checkbox" value="y" name="home_page" id="home_page" <?= $is_home_page ?> />
-                <label for="allow_remark">设为首页，同时原默认首页将变更为：<?= BLOG_URL ?>posts</label>
+        </div>
+        <div class="col-xl-3">
+            <div class="shadow-sm p-3 mb-2 bg-white rounded">
+                <div class="form-group">
+                    <label>链接别名：（用于seo设置 <a href="./setting.php?action=seo">&rarr;</a>）</label>
+                    <input name="alias" id="alias" class="form-control" value="<?= $alias ?>"/>
+                </div>
+                <div class="form-group">
+                    <label>跳转链接：<small class="text-muted">（填写后不展示页面内容，直接跳转该地址）</small></label>
+                    <input name="link" id="link" type="url" class="form-control" value="<?= $link ?>" placeholder="https://"/>
+                </div>
+                <div class="form-group">
+                    <label>页面模板：</label>
+                    <?php if ($customTemplates):
+                        $sortListHtml = '<option value="">默认</option>';
+                        foreach ($customTemplates as $v) {
+                            $select = $v['filename'] == $template ? 'selected="selected"' : '';
+                            $sortListHtml .= '<option value="' . str_replace('.php', '', $v['filename']) . '" ' . $select . '>' . ($v['comment']) . '</option>';
+                        }
+                        ?>
+                        <select id="template" name="template" class="form-control"><?= $sortListHtml; ?></select>
+                        <small class="form-text text-muted">(选择当前模板支持的页面模板，可不选)</small>
+                    <?php else: ?>
+                        <input class="form-control" id="template" name="template" value="<?= $template ?>">
+                        <small class="form-text text-muted">(用于自定义页面模板，对应模板目录下xxx.php文件，xxx即为模板名，可不填)</small>
+                    <?php endif; ?>
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" value="y" name="allow_remark" id="allow_remark" <?= $is_allow_remark ?> />
+                    <label for="allow_remark">允许评论</label>
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" value="y" name="home_page" id="home_page" <?= $is_home_page ?> />
+                    <label for="allow_remark">设为首页，<small class="text-muted">原首页：<?= BLOG_URL ?>posts</small></label>
+                </div>
             </div>
             <div id="post_button">
                 <input type="hidden" name="ishide" id="ishide" value="<?= $hide ?>"/>
                 <input type="hidden" name="pageid" value="<?= $pageId ?>"/>
                 <?php if ($pageId < 0): ?>
-                    <input type="submit" value="发布页面" onclick="return checkform();" class="btn btn-sm btn-success"/>
+                    <input type="submit" value="发布页面" onclick="return checkform();" class="btn btn-success"/>
                 <?php else: ?>
-                    <input type="submit" value="保存并返回" onclick="return checkform();" class="btn btn-sm btn-success"/>
+                    <input type="submit" value="保存并返回" onclick="return checkform();" class="btn btn-success"/>
                 <?php endif ?>
             </div>
         </div>
@@ -123,30 +126,24 @@
             width: "100%",
             height: 640,
             toolbarIcons: function () {
-                return ["undo", "redo", "|",
-                    "bold", "del", "italic", "quote", "|",
+                return ["bold", "del", "italic", "quote", "|",
                     "h1", "h2", "h3", "|",
                     "list-ul", "list-ol", "hr", "|",
-                    "link", "image", "preformatted-text", "table", "|", "watch", "help"]
+                    "link", "image", "preformatted-text", "table", "|", "preview", "help"]
             },
             path: "editor.md/lib/",
             tex: false,
             flowChart: false,
             watch: false,
             htmlDecode: true,
+            lineNumbers: false,
             sequenceDiagram: false,
             syncScrolling: "single",
             onload: function () {
                 hooks.doAction("page_loaded", this);
-                //在大屏模式下，编辑器默认显示预览
-                if ($(window).width() > 767) {
-                    this.watch();
-                }
             }
         });
         Editor.setToolbarAutoFixed(false);
-
-
     });
     // 离开页面时，如果页面内容已做修改，则询问用户是否离开
     var pageText;
@@ -159,7 +156,6 @@
         if (e) e.returnValue = '离开页面提示';
         return '离开页面提示';
     }
-
     // 页面编辑界面全局快捷键 Ctrl（Cmd）+ S 保存内容
     document.addEventListener('keydown', function (e) {
         if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
