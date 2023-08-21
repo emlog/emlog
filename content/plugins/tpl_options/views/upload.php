bead96774bea71de7e804916ec8740118d1b2b02
snow <emloog@gmail.com>
2023-08-21 18:57:57 +0800

    fix:修复主题设置插件模板未限制直接访问的问题

--- content/plugins/tpl_options/views/upload.php
+++ content/plugins/tpl_options/views/upload.php
@@ -1,3 +1,6 @@
+<?php
+!defined('EMLOG_ROOT') && exit('access deined!');
+?>
 <script>
     parent.setImage('<?php echo $src; ?>', '<?php echo $path; ?>', <?php echo $code; ?>, '<?php echo $this->encode($msg); ?>');
 </script>
