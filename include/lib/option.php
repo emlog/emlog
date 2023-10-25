abb237adac5798502e1d2ed1a58c2f0a939ee218
snow <emloog@gmail.com>
2023-10-25 17:42:24 +0800

    release 2.2.2

--- include/lib/option.php
+++ include/lib/option.php
@@ -7,8 +7,8 @@
 
 class Option {
 
-    const EMLOG_VERSION = 'pro 2.2.1';
-    const EMLOG_VERSION_TIMESTAMP = 1697365313;
+    const EMLOG_VERSION = 'pro 2.2.2';
+    const EMLOG_VERSION_TIMESTAMP = 1698226919;
     const UPLOADFILE_PATH = '../content/uploadfile/';
     const UPLOADFILE_FULL_PATH = EMLOG_ROOT . '/content/uploadfile/';
 
