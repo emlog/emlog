41d9e68a4fc70d79a31de7bbae874b0a6fbc98c8
snow <emloog@gmail.com>
2023-11-25 12:59:45 +0800

    feat: api和用户信息设置采用 ajax

--- admin/views/blogger.php
+++ admin/views/blogger.php
@@ -1,22 +1,4 @@
 <?php defined('EMLOG_ROOT') || exit('access denied!'); ?>
-<?php if (isset($_GET['active_edit'])): ?>
-    <div class="alert alert-success">资料修改成功</div><?php endif ?>
-<?php if (isset($_GET['active_del'])): ?>
-    <div class="alert alert-success">头像删除成功</div><?php endif ?>
-<?php if (isset($_GET['error_a'])): ?>
-    <div class="alert alert-danger">昵称不能为空</div><?php endif ?>
-<?php if (isset($_GET['error_email'])): ?>
-    <div class="alert alert-danger">请正确填写邮箱</div><?php endif ?>
-<?php if (isset($_GET['error_c'])): ?>
-    <div class="alert alert-danger">密码长度不得小于6位</div><?php endif ?>
-<?php if (isset($_GET['error_d'])): ?>
-    <div class="alert alert-danger">两次输入的密码不一致</div><?php endif ?>
-<?php if (isset($_GET['error_e'])): ?>
-    <div class="alert alert-danger">该登录名已被占用</div><?php endif ?>
-<?php if (isset($_GET['error_f'])): ?>
-    <div class="alert alert-danger">该昵称已被占用</div><?php endif ?>
-<?php if (isset($_GET['error_g'])): ?>
-    <div class="alert alert-danger">该邮箱已被占用</div><?php endif ?>
 <?php if (User::isAdmin()): ?>
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">设置</h1>
