477aecd05daf4dc106f636b0eccfa98e9f2109fd
snow <emloog@gmail.com>
2023-11-26 17:30:52 +0800

    feat:优化更新流程

--- admin/views/index.php
+++ admin/views/index.php
@@ -248,8 +248,6 @@
             updateModalLoading.addClass("spinner-border text-primary");
             updateModalMsg.html("更新中... 请耐心等待");
             updateModalChanges.html("");
-            upbtn.text("更新中...")
-            upbtn.attr('href', 'javascript:void(0);');
 
             $.get(`./upgrade.php?action=update&source=${source}&upsql=${upSQL}`, function (data) {
                 upmsg.removeClass();
