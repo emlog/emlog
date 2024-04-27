2bc0b1b21c2bab3ea0523d99158c7c6a027be493
snow <emloog@gmail.com>
2024-04-27 23:09:29 +0800

    feat:Optimize UI display

--- admin/views/uc_footer.php
+++ admin/views/uc_footer.php
@@ -1,7 +1,7 @@
 <?php defined('EMLOG_ROOT') || exit('access denied!'); ?>
 <footer class="py-4">
     <div class="text-center">
-        <span>Copyright © <?= date("Y") ?> </span>
+        <small>© <?= date("Y") ?> <?= Option::get('blogname') ?> </small>
     </div>
 </footer>
 </div>
