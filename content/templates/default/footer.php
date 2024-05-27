6e730216830a6b4cc02df574da04d55042248921
snow <emloog@gmail.com>
2024-05-27 16:38:44 +0800

    feat: Optimize default theme add dark mode

--- content/templates/default/footer.php
+++ content/templates/default/footer.php
@@ -15,7 +15,7 @@ defined('EMLOG_ROOT') || exit('access denied!');
         <?php doAction('index_footer') ?>
     </div>
 </footer>
-<script src="<?= TEMPLATE_URL ?>js/common_tpl.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
+<script src="<?= TEMPLATE_URL ?>js/common_tpl.js?v=1716799087&t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
 <script src="<?= TEMPLATE_URL ?>js/zoom.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
 </body>
 </html>
