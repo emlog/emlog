67e6bb7e5a1342f6268df34cf82ddac2a0a75aee
snow <4344235+emlog@users.noreply.github.com>
2024-05-31 17:15:05 +0800

    feat:Add download ranking

--- admin/views/store_plu.php
+++ admin/views/store_plu.php
@@ -20,8 +20,8 @@
         <a href="./store.php?action=plu&tag=free" class="badge badge-success m-1 p-2">仅看免费</a>
         <a href="./store.php?action=plu&tag=paid" class="badge badge-warning m-1 p-2">仅看付费</a>
         <a href="./store.php?action=plu&tag=promo" class="badge badge-danger m-1 p-2">限时优惠</a>
-        <a href="./store.php?action=plu&tag=free_top" class="badge badge-light text-primary m-1 p-2 small">免费排行榜</a>
-        <a href="./store.php?action=plu&tag=paid_top" class="badge badge-light text-primary m-1 p-2 small">付费排行榜</a>
+        <a href="./store.php?action=plu&tag=download_top" class="badge badge-light text-primary m-1 p-2 small">下载排行榜</a>
+        <a href="./store.php?action=plu&tag=paid_top" class="badge badge-light text-primary m-1 p-2 small">购买排行榜</a>
     </div>
     <div class="d-flex mb-3 mb-sm-0">
         <form action="#" method="get" class="mr-sm-2">
