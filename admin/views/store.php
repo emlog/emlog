a61b7fc8ce0ae2158ef78edd79244cea4b9f9500
snow <emloog@gmail.com>
2024-07-12 13:50:05 +0800

    feat:Add the feat of tagging articles

--- admin/views/store.php
+++ admin/views/store.php
@@ -18,11 +18,14 @@
 <div class="d-flex flex-column flex-sm-row justify-content-between mb-4 ml-1">
     <div class="mb-3 mb-sm-0">
         <a href="./store.php" class="badge badge-success m-1 p-2">全部</a>
-        <a href="./store.php?tag=free" class="badge badge-success m-1 p-2">仅看免费</a>
-        <a href="./store.php?tag=paid" class="badge badge-warning m-1 p-2">仅看付费</a>
-        <a href="./store.php?tag=promo" class="badge badge-danger m-1 p-2">限时优惠</a>
-        <a href="./store.php?tag=download_top" class="badge badge-light text-primary m-1 p-2 small">🔥下载排行榜</a>
-
+        <a href="./store.php?tag=free" class="badge badge-success m-1 p-2">免费</a>
+        <a href="./store.php?tag=paid" class="badge badge-warning m-1 p-2">付费</a>
+        <a href="./store.php?tag=promo" class="badge badge-danger m-1 p-2">优惠</a>
+        <a href="./store.php?tag=download_top" class="badge badge-light text-primary m-1 p-2 small">🔥下载排行</a>
+        <a href="./store.php?sid=2" class="badge badge-light text-primary m-1 p-2 small">SEO</a>
+        <a href="./store.php?sid=1" class="badge badge-light text-primary m-1 p-2 small">资源下载</a>
+        <a href="./store.php?sid=12" class="badge badge-light text-primary m-1 p-2 small">内容运营</a>
+        <a href="./store.php?sid=11" class="badge badge-light text-primary m-1 p-2 small">用户互动</a>
     </div>
     <div class="d-flex mb-3 mb-sm-0">
         <form action="#" method="get" class="mr-sm-2">
