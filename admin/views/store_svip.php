6e5e778cee0d5df927c95cfb5c6fd82031a82555
snow <emloog@gmail.com>
2024-07-15 11:04:50 +0800

    feat: Refactor

--- admin/views/store_svip.php
+++ admin/views/store_svip.php
@@ -38,7 +38,7 @@
                             <?php endif; ?>
                         </p>
                         <p class="card-text text-muted small">
-                            开发者：<?= $v['author'] ?><br>
+                            开发者：<a href="./store.php?author_id=<?= $v['author_id'] ?>"><?= $v['author'] ?></a><br>
                             版本号：<?= $v['ver'] ?><br>
                             下载次数：<?= $v['downloads'] ?><br>
                             更新时间：<?= $v['update_time'] ?><br>
