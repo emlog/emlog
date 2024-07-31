df7b4384ccda9dff02fc518a9e90c492858e2c51
snow <emloog@gmail.com>
2024-07-31 09:13:29 +0800

    fix:Adjust update script logic

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -38,9 +38,41 @@ if ($action === 'update' && User::isAdmin()) {
     $source = isset($_GET['source']) ? trim($_GET['source']) : '';
     $upsql = isset($_GET['upsql']) ? trim($_GET['upsql']) : '';
 
-    if (empty($source)) {
+    if (empty($source) || empty($upsql)) {
         exit('error');
     }
+
+    // update database
+    $temp_sql_file = emFetchFile($upsql);
+    if (!$temp_sql_file) {
+        exit('error_down');
+    }
+    $DB = Database::getInstance();
+    $setchar = "ALTER DATABASE `" . DB_NAME . "` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
+    $sql = file($temp_sql_file);
+    array_unshift($sql, $setchar);
+    $query = '';
+    foreach ($sql as $value) {
+        // 只执行当前版本需要的更新
+        if (!empty($value) && $value[0] == '#') {
+            preg_match("/#\s(pro\s[\.\d]+)/i", $value, $v);
+            $ver = isset($v[1]) ? trim($v[1]) : '';
+            if (version_compare(Option::EMLOG_VERSION, $ver) > 0) {
+                break;
+            }
+        }
+        if (!$value || $value[0] == '#') {
+            continue;
+        }
+        $value = str_replace("{db_prefix}", DB_PREFIX, trim($value));
+        $query .= $value;
+        if (preg_match("/\;$/i", $value)) {
+            $DB->query($query, 1);
+            $query = '';
+        }
+    }
+    $CACHE->updateCache();
+    @unlink($temp_sql_file);
 
     // update files
     $temp_zip_file = emFetchFile($source);
@@ -57,38 +89,5 @@ if ($action === 'update' && User::isAdmin()) {
     }
     @unlink($temp_zip_file);
 
-    // update database
-    if ($upsql) {
-        $temp_sql_file = emFetchFile($upsql);
-        if (!$temp_sql_file) {
-            exit('error_down');
-        }
-        $DB = Database::getInstance();
-        $setchar = "ALTER DATABASE `" . DB_NAME . "` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
-        $sql = file($temp_sql_file);
-        array_unshift($sql, $setchar);
-        $query = '';
-        foreach ($sql as $value) {
-            // 只执行当前版本需要的更新
-            if (!empty($value) && $value[0] == '#') {
-                preg_match("/#\s(pro\s[\.\d]+)/i", $value, $v);
-                $ver = isset($v[1]) ? trim($v[1]) : '';
-                if (version_compare(Option::EMLOG_VERSION, $ver) > 0) {
-                    break;
-                }
-            }
-            if (!$value || $value[0] == '#') {
-                continue;
-            }
-            $value = str_replace("{db_prefix}", DB_PREFIX, trim($value));
-            $query .= $value;
-            if (preg_match("/\;$/i", $value)) {
-                $DB->query($query, 1);
-                $query = '';
-            }
-        }
-        $CACHE->updateCache();
-        @unlink($temp_sql_file);
-    }
     exit('succ');
 }
