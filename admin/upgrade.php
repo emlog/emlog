d65d39a6bfffc41f4da9610902c786bcea649382
snow <emloog@gmail.com>
2024-09-11 11:04:06 +0800

    fix:hk server install app timeout

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -21,7 +21,7 @@ if ($action === 'check_update') {
         'timestamp' => Option::EMLOG_VERSION_TIMESTAMP,
     ]);
 
-    $emcurl->request('https://www.emlog.net/service/upgrade');
+    $emcurl->request('https://store.emlog.net/service/upgrade');
     $retStatus = $emcurl->getHttpStatus();
     $response = $emcurl->getRespone();
     header('Content-Type: application/json; charset=UTF-8');
--- include/model/store_model.php
+++ include/model/store_model.php
@@ -1,37 +1,46 @@
 <?php
+
 /**
  * store model
  * @package EMLOG
  * @link https://www.emlog.net
  */
 
-class Store_Model {
+class Store_Model
+{
 
-    public function getApps($tag, $keyword, $page, $author_id, $sid) {
+    public function getApps($tag, $keyword, $page, $author_id, $sid)
+    {
         return $this->reqEmStore('all', $tag, $keyword, $page, $author_id, $sid);
     }
 
-    public function getTemplates($tag, $keyword, $page, $author_id, $sid) {
+    public function getTemplates($tag, $keyword, $page, $author_id, $sid)
+    {
         return $this->reqEmStore('tpl', $tag, $keyword, $page, $author_id, $sid);
     }
 
-    public function getPlugins($tag, $keyword, $page, $author_id, $sid) {
+    public function getPlugins($tag, $keyword, $page, $author_id, $sid)
+    {
         return $this->reqEmStore('plu', $tag, $keyword, $page, $author_id, $sid);
     }
 
-    public function getMyAddon() {
+    public function getMyAddon()
+    {
         return $this->reqEmStore('mine');
     }
 
-    public function getSvipAddon() {
+    public function getSvipAddon()
+    {
         return $this->reqEmStore('svip');
     }
 
-    public function getTopAddon() {
+    public function getTopAddon()
+    {
         return $this->reqEmStore('top');
     }
 
-    public function reqEmStore($type, $tag = '', $keyword = '', $page = 1, $author_id = 0, $sid = 0) {
+    public function reqEmStore($type, $tag = '', $keyword = '', $page = 1, $author_id = 0, $sid = 0)
+    {
         $emcurl = new EmCurl();
 
         $post_data = [
@@ -45,7 +54,7 @@ class Store_Model {
             'sid'       => $sid
         ];
         $emcurl->setPost($post_data);
-        $emcurl->request('https://www.emlog.net/store/pro');
+        $emcurl->request('https://store.emlog.net/store/pro');
 
         $retStatus = $emcurl->getHttpStatus();
         if ($retStatus !== MSGCODE_SUCCESS) {
@@ -88,5 +97,4 @@ class Store_Model {
         }
         return $data;
     }
-
 }
