3af6b63ab33f764d810bdccab1b18943f99e53f0
snow <4344235+emlog@users.noreply.github.com>
2025-03-24 09:45:07 +0800

    优化个人中心界面

--- admin/views/uc_footer.php
+++ admin/views/uc_footer.php
@@ -1,13 +1,13 @@
 <?php defined('EMLOG_ROOT') || exit('access denied!'); ?>
-<footer class="py-4">
-    <div class="text-center">
-        <small>© <?= date("Y") ?> <?= Option::get('blogname') ?> </small>
-    </div>
-</footer>
 </div>
 </div>
 </div>
 </main>
+<footer class="py-3">
+    <div class="text-center">
+        <small>© <?= date("Y") ?> <?= Option::get('blogname') ?> </small>
+    </div>
+</footer>
 <?php doAction('adm_footer') ?>
 <script src="./views/js/sb-admin-2.min.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
 </body>
