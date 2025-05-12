87c9f9f589ca70555f9072afdf5109485b9f54d1
snow <4344235+emlog@users.noreply.github.com>
2025-05-12 21:30:36 +0800

    chore: update EMLOG version and timestamp to 2.5.14

--- include/lib/option.php
+++ include/lib/option.php
@@ -8,8 +8,8 @@
 
 class Option
 {
-    const EMLOG_VERSION = 'pro 2.5.13';
-    const EMLOG_VERSION_TIMESTAMP = 1745404804;
+    const EMLOG_VERSION = 'pro 2.5.14';
+    const EMLOG_VERSION_TIMESTAMP = 1747056571;
     const UPLOADFILE_PATH = '../content/uploadfile/';
     const UPLOADFILE_FULL_PATH = EMLOG_ROOT . '/content/uploadfile/';
 
