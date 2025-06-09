ff5dfcb950be1dd215c2d85f9044e7816c09bf0f
snow <4344235+emlog@users.noreply.github.com>
2025-06-09 20:39:51 +0800

    fix(plugin_controller): add plugin active check before loading

--- include/controller/plugin_controller.php
+++ include/controller/plugin_controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * loading plug-in page
  *
@@ -6,9 +7,14 @@
  * @link https://www.emlog.net
  */
 
-class Plugin_Controller {
-    function loadPluginShow($params) {
+class Plugin_Controller
+{
+    function loadPluginShow($params)
+    {
         $plugin = isset($params[1]) && $params[1] == 'plugin' ? addslashes($params[2]) : '';
+        if (Plugin::isActive($plugin) === false) {
+            show_404_page();
+        }
         if (preg_match("/^[\w\-]+$/", $plugin) && file_exists(EMLOG_ROOT . "/content/plugins/{$plugin}/{$plugin}_show.php")) {
             include_once("./content/plugins/{$plugin}/{$plugin}_show.php");
         }
