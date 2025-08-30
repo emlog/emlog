3011b132e958684cebb2e83eb7d48424b006dbf6
snow <4344235+emlog@users.noreply.github.com>
2025-08-30 19:51:55 +0800

    fix(media): correct modal div structure and indentation

--- admin/views/media.php
+++ admin/views/media.php
@@ -256,194 +256,195 @@
             }
         </script>
     </div>
+</div>
 
-    <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content border-0 shadow">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title" id="exampleModalLabel">上传图片/文件</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+<div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content border-0 shadow">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="exampleModalLabel">上传图片/文件</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="./media.php?action=upload<?= '&sid=' . $sid ?>" class="dropzone" id="up-form"></form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade bd-example-modal-lg" id="mediaSortModal" tabindex="-1" role="dialog" aria-labelledby="mediaSortModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content border-0 shadow">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="mediaSortModalLabel">添加资源分类</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="media.php?action=add_media_sort" method="post">
                 <div class="modal-body">
-                    <form action="./media.php?action=upload<?= '&sid=' . $sid ?>" class="dropzone" id="up-form"></form>
+                    <div class="form-group">
+                        <label for="sortname">分类名称</label>
+                        <input class="form-control" id="sortname" maxlength="255" name="sortname" required>
+                    </div>
                 </div>
-            </div>
+                <div class="modal-footer border-0">
+                    <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
+                    <button type="submit" class="btn btn-sm btn-success">保存</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <div class="modal fade bd-example-modal-lg" id="mediaSortModal" tabindex="-1" role="dialog" aria-labelledby="mediaSortModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content border-0 shadow">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title" id="mediaSortModalLabel">添加资源分类</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="media.php?action=add_media_sort" method="post">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="sortname">分类名称</label>
-                            <input class="form-control" id="sortname" maxlength="255" name="sortname" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer border-0">
-                        <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
-                        <button type="submit" class="btn btn-sm btn-success">保存</button>
-                    </div>
-                </form>
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content border-0 shadow">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="exampleModalLabel">修改资源分类</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form method="post" action="media.php?action=update_media_sort">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="sortname" name="sortname" required>
+                    </div>
+                </div>
+                <div class="modal-footer border-0">
+                    <input type="hidden" value="" id="id" name="id" />
+                    <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
+                    <button type="submit" class="btn btn-sm btn-success">保存</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content border-0 shadow">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title" id="exampleModalLabel">修改资源分类</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form method="post" action="media.php?action=update_media_sort">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="sortname" name="sortname" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer border-0">
-                        <input type="hidden" value="" id="id" name="id" />
-                        <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
-                        <button type="submit" class="btn btn-sm btn-success">保存</button>
-                    </div>
-                </form>
+<div class="modal fade" id="editMediaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content border-0 shadow">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="exampleModalLabel">编辑资源</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form method="post" action="media.php?action=update_media">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="filename">资源名称</label>
+                        <input type="text" class="form-control" id="filename" name="filename" required>
+                    </div>
+                </div>
+                <div class="modal-footer border-0">
+                    <input type="hidden" value="" id="id" name="id" />
+                    <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
+                    <button type="submit" class="btn btn-sm btn-success">保存</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="editMediaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content border-0 shadow">
-                <div class="modal-header border-0">
-                    <h5 class="modal-title" id="exampleModalLabel">编辑资源</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form method="post" action="media.php?action=update_media">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="filename">资源名称</label>
-                            <input type="text" class="form-control" id="filename" name="filename" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer border-0">
-                        <input type="hidden" value="" id="id" name="id" />
-                        <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">取消</button>
-                        <button type="submit" class="btn btn-sm btn-success">保存</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <script src="./views/js/dropzone.min.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
-    <link rel="stylesheet" type="text/css" href="./views/components/highslide/highslide.css?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>" />
-    <script src="./views/components/highslide/highslide.min.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
-    <script>
-        Dropzone.options.upForm = {
-            paramName: "file",
-            maxFilesize: 20480, // 20G
-            timeout: 3600000,
-            init: function() {
-                this.on("error", function(file, response) {
-                    // alert(response);
-                });
-            }
-        };
-        $(function() {
-            $("#menu_media").addClass('active');
-            setTimeout(hideActived, 3600);
-            $('#exampleModal').on('hidden.bs.modal', function(e) {
-                window.location.reload();
+<script src="./views/js/dropzone.min.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
+<link rel="stylesheet" type="text/css" href="./views/components/highslide/highslide.css?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>" />
+<script src="./views/components/highslide/highslide.min.js?t=<?= Option::EMLOG_VERSION_TIMESTAMP ?>"></script>
+<script>
+    Dropzone.options.upForm = {
+        paramName: "file",
+        maxFilesize: 20480, // 20G
+        timeout: 3600000,
+        init: function() {
+            this.on("error", function(file, response) {
+                // alert(response);
             });
-
-            $('#editModal').on('show.bs.modal', function(event) {
-                var button = $(event.relatedTarget)
-                var sortname = button.data('sortname')
-                var id = button.data('id')
-                var modal = $(this)
-                modal.find('.modal-body input').val(sortname)
-                modal.find('.modal-footer input').val(id)
-            })
-
-            $('#editMediaModal').on('show.bs.modal', function(event) {
-                var button = $(event.relatedTarget)
-                var filename = button.data('filename')
-                var id = button.data('id')
-                var modal = $(this)
-                modal.find('.modal-body input').val(filename)
-                modal.find('.modal-footer input').val(id)
-            })
-
-            if (window.outerWidth > 767) {
-                hs.graphicsDir = './views/components/highslide/graphics/';
-                hs.wrapperClassName = 'rounded-white';
-            } else {
-                $('.highslide').removeAttr('onclick') // 如果是移动端，则不使用 highslide 功能
-            }
-
-            // copy url
-            $('.copy-link').click(function(e) {
-                e.preventDefault();
-                var link = $(this).data('url');
-                navigator.clipboard.writeText(link);
-                $(this).popover({
-                    content: '地址已复制',
-                    placement: 'top',
-                    trigger: 'manual'
-                }).popover('show');
-                setTimeout(() => $(this).popover('hide'), 1000);
-            });
+        }
+    };
+    $(function() {
+        $("#menu_media").addClass('active');
+        setTimeout(hideActived, 3600);
+        $('#exampleModal').on('hidden.bs.modal', function(e) {
+            window.location.reload();
         });
 
-        // 日期选择器关闭的回调函数
-        function onDatepickerClose(dateText, inst) {
-            if (dateText) {
-                var date = new Date(dateText);
-                var formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-                var url = 'media.php?date=' + formattedDate;
-                window.location.href = url;
-            }
+        $('#editModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var sortname = button.data('sortname')
+            var id = button.data('id')
+            var modal = $(this)
+            modal.find('.modal-body input').val(sortname)
+            modal.find('.modal-footer input').val(id)
+        })
+
+        $('#editMediaModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var filename = button.data('filename')
+            var id = button.data('id')
+            var modal = $(this)
+            modal.find('.modal-body input').val(filename)
+            modal.find('.modal-footer input').val(id)
+        })
+
+        if (window.outerWidth > 767) {
+            hs.graphicsDir = './views/components/highslide/graphics/';
+            hs.wrapperClassName = 'rounded-white';
+        } else {
+            $('.highslide').removeAttr('onclick') // 如果是移动端，则不使用 highslide 功能
         }
 
-        function mediaact(act) {
-            if (getChecked('aids') === false) {
-                infoAlert('请选择要删除的资源');
-                return;
-            }
+        // copy url
+        $('.copy-link').click(function(e) {
+            e.preventDefault();
+            var link = $(this).data('url');
+            navigator.clipboard.writeText(link);
+            $(this).popover({
+                content: '地址已复制',
+                placement: 'top',
+                trigger: 'manual'
+            }).popover('show');
+            setTimeout(() => $(this).popover('hide'), 1000);
+        });
+    });
 
-            if (act === 'del') {
-                delAlert2('', '删除所选资源文件？', function() {
-                    $("#operate").val(act);
-                    $("#form_media").submit();
-                })
-                return;
-            }
-            $("#operate").val(act);
-            $("#form_media").submit();
+    // 日期选择器关闭的回调函数
+    function onDatepickerClose(dateText, inst) {
+        if (dateText) {
+            var date = new Date(dateText);
+            var formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+            var url = 'media.php?date=' + formattedDate;
+            window.location.href = url;
+        }
+    }
+
+    function mediaact(act) {
+        if (getChecked('aids') === false) {
+            infoAlert('请选择要删除的资源');
+            return;
         }
 
-        // 更改分类
-        function changeSort(obj) {
-            if (getChecked('aids') === false) {
-                infoAlert('请选择要移动的资源');
-                return;
-            }
-            if ($('#sort').val() === '') return;
-            $("#operate").val('move');
-            $("#form_media").submit();
+        if (act === 'del') {
+            delAlert2('', '删除所选资源文件？', function() {
+                $("#operate").val(act);
+                $("#form_media").submit();
+            })
+            return;
         }
-    </script>
+        $("#operate").val(act);
+        $("#form_media").submit();
+    }
+
+    // 更改分类
+    function changeSort(obj) {
+        if (getChecked('aids') === false) {
+            infoAlert('请选择要移动的资源');
+            return;
+        }
+        if ($('#sort').val() === '') return;
+        $("#operate").val('move');
+        $("#form_media").submit();
+    }
+</script>
