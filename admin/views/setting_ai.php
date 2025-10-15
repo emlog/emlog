08f0765eb68eb06afd11fbe7c9cfef3cbf33db93
snow <4344235+emlog@users.noreply.github.com>
2025-10-15 08:04:54 +0800

    docs(settings): update AI model options in settings page

--- admin/views/setting_ai.php
+++ admin/views/setting_ai.php
@@ -214,17 +214,17 @@
             <a href="https://www.deepseek.com/" target="_blank">DeepSeek</a><br>
             API URL：https://api.deepseek.com/v1/chat/completions<br>
             API Key：<a href="https://platform.deepseek.com/api_keys" target="_blank">生成api key</a>，格式如：sk-****<br>
-            Model：deepseek-chat、deepseek-reasoner<br>
+            Model：deepseek-chat<br>
             <hr>
             <a href="https://bigmodel.cn/" target="_blank">智谱AI</a><br>
             API URL：https://open.bigmodel.cn/api/paas/v4/chat/completions<br>
             API Key：<a href="https://bigmodel.cn/usercenter/proj-mgmt/apikeys" target="_blank">生成api key</a><br>
-            Model：glm-4.5、glm-4.5-flash (免费)<br>
+            Model：glm-4.6、glm-4.5、glm-4.5-flash<br>
             <hr>
             <a href="https://cloud.siliconflow.cn/" target="_blank">硅基流动</a><br>
             API URL：https://api.siliconflow.cn/v1/chat/completions<br>
             API Key：<a href="https://cloud.siliconflow.cn/me/account/ak" target="_blank">生成api key</a><br>
-            Model：Qwen/Qwen3-8B (免费)、THUDM/GLM-4-9B-0414 (免费)<br>
+            Model：Qwen/Qwen3-8B、THUDM/GLM-4-9B-0414<br>
             <hr>
             支持 OpenAI 协议的大模型，<a href="https://www.emlog.net/docs/ai/ai_emlog" target="_blank">更多AI模型</a><br>
         `;
@@ -236,7 +236,7 @@
             <a href="https://cloud.siliconflow.cn/" target="_blank">硅基流动</a><br>
             API URL：https://api.siliconflow.cn/v1/images/generations<br>
             API Key：<a href="https://cloud.siliconflow.cn/me/account/ak" target="_blank">生成api key</a><br>
-            Model：Kwai-Kolors/Kolors (免费)<br>
+            Model：Kwai-Kolors/Kolors<br>
             <hr>
             <a href="https://console.volcengine.com/auth/login?redirectURI=%2Fark" target="_blank">豆包</a><br>
             API URL：https://ark.cn-beijing.volces.com/api/v3/images/generations<br>
