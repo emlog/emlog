bd0f626ef5343ae5edf57dff024a4ab05be206ce
snow <4344235+emlog@users.noreply.github.com>
2025-12-23 22:28:50 +0800

    refactor(language): reorganize language files into subdirectories and update EmLa

--- include/lib/emlang.php
+++ include/lib/emlang.php
@@ -8,9 +8,11 @@
 
 class EmLang
 {
-    private static $_instance = null;
-    private $_currentLanguage = 'zh_CN';
-    private $_langData = [];
+    private static $_instance         = null;
+    private        $_currentLanguage  = 'zh_CN';
+    private        $_langData         = [];
+    private        $_langTemplateData = [];
+    private        $_langPluginData   = [];
 
     private function __construct()
     {
@@ -39,11 +41,29 @@ class EmLang
 
     private function loadLangFile()
     {
-        $langFile = EMLOG_ROOT . '/content/languages/' . $this->_currentLanguage . '.php';
+        $langFile = EMLOG_ROOT . '/content/languages/' . $this->_currentLanguage . '/' . $this->_currentLanguage . '.php';
         if (file_exists($langFile)) {
             $this->_langData = include $langFile;
         } else {
-            $this->_langData = include EMLOG_ROOT . '/content/languages/zh_CN.php';
+            $this->_langData = include EMLOG_ROOT . '/content/languages/zh_CN/zh_CN.php';
+        }
+    }
+
+    public function loadTemplateLangFile()
+    {
+        $langFile = TEMPLATE_PATH . '/languages/' . $this->_currentLanguage . '/' . $this->_currentLanguage . '.php';
+        if (file_exists($langFile)) {
+            $this->_langTemplateData = include $langFile;
+        }
+    }
+
+    public function loadPluginLangFile($pluginName = '')
+    {
+        if (!empty($pluginName)) {
+            $langFile = PLUGIN_PATH . $pluginName . '/languages/' . $this->_currentLanguage . '/' . $this->_currentLanguage . '.php';
+            if (file_exists($langFile)) {
+                $this->_langPluginData = include $langFile;
+            }
         }
     }
 
@@ -52,9 +72,16 @@ class EmLang
         return isset($this->_langData[$key]) ? $this->_langData[$key] : $key;
     }
 
-    public function getAll()
+    public function getTpl($key)
     {
-        return $this->_langData;
+        $this->loadTemplateLangFile();
+        return isset($this->_langTemplateData[$key]) ? $this->_langTemplateData[$key] : $key;
+    }
+
+    public function getPlu($key, $pluginName = '')
+    {
+        $this->loadPluginLangFile($pluginName);
+        return isset($this->_langPluginData[$key]) ? $this->_langPluginData[$key] : $key;
     }
 
     public function currentLang()
@@ -63,10 +90,17 @@ class EmLang
     }
 }
 
-/**
- * Display translated string
- */
 function _lang($key)
 {
     return EmLang::getInstance()->get($key);
+}
+
+function _langTpl($key)
+{
+    return EmLang::getInstance()->getTpl($key);
+}
+
+function _langPlu($key, $pluginName = '')
+{
+    return EmLang::getInstance()->getPlu($key, $pluginName);
 }
