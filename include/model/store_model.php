b39449416826620c4f9ef0e6cc156e1a5ef264ea
snow <4344235+emlog@users.noreply.github.com>
2025-12-26 12:12:05 +0800

    fix: update store API endpoints from www to store subdomain

--- include/model/store_model.php
+++ include/model/store_model.php
@@ -50,7 +50,7 @@ class Store_Model
         ];
 
         $emcurl->setPost($post_data);
-        $emcurl->request('https://www.emlog.net/store/favoritesAdd');
+        $emcurl->request('https://store.emlog.net/store/favoritesAdd');
 
         $retStatus = $emcurl->getHttpStatus();
         if ($retStatus !== MSGCODE_SUCCESS) {
@@ -78,7 +78,7 @@ class Store_Model
         ];
 
         $emcurl->setPost($post_data);
-        $emcurl->request('https://www.emlog.net/store/favoritesRemove');
+        $emcurl->request('https://store.emlog.net/store/favoritesRemove');
 
         $retStatus = $emcurl->getHttpStatus();
         if ($retStatus !== MSGCODE_SUCCESS) {
