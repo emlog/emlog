4d2c2d1966c03a0d31a7040e51a260346d36a711
snow <4344235+emlog@users.noreply.github.com>
2026-01-19 09:05:56 +0800

    release version 2.6.2

--- include/lib/option.php
+++ include/lib/option.php
@@ -8,8 +8,8 @@
 
 class Option
 {
-    const EMLOG_VERSION = 'pro 2.6.1';
-    const EMLOG_VERSION_TIMESTAMP = 1767274904;
+    const EMLOG_VERSION = 'pro 2.6.2';
+    const EMLOG_VERSION_TIMESTAMP = 1768784756;
     const UPLOADFILE_PATH = '../content/uploadfile/';
     const UPLOADFILE_FULL_PATH = EMLOG_ROOT . '/content/uploadfile/';
 
