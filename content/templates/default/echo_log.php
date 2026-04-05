164e7c5e8dfb30f3cb0b112e7da80e3ecf1a289d
snow <4344235+emlog@users.noreply.github.com>
2026-04-05 18:19:55 +0800

    style(templates): improve blog post header layout and responsiveness

--- content/templates/default/echo_log.php
+++ content/templates/default/echo_log.php
@@ -7,14 +7,24 @@ defined('EMLOG_ROOT') || exit('access denied!');
 ?>
 <article class="container log-con blog-container">
     <span class="back-top mh" onclick="history.go(-1);">&laquo;</span>
-    <h1 class="log-title"><?php topflg($top) ?><?= $log_title ?></h1>
-    <p class="date">
-        <?php blog_author($author) ?> <?= _langTpl('published_on') ?>
-        <time class="m-r-5"><?= date('Y-n-j H:i', $date) ?></time>
-        <span class="m-r-5"><?= _langTpl('views') ?><?= $views ?></span>
-        <span class="m-r-5"><?php blog_sort($sortid) ?></span>
-        <?php editflg($logid, $author) ?>
-    </p>
+    <header class="log-header">
+        <h1 class="log-title"><?php topflg($top) ?><?= $log_title ?></h1>
+        <div class="log-meta">
+            <div class="log-meta-left">
+                <span class="log-meta-item"><?php blog_author($author) ?></span>
+                <span class="log-meta-item">
+                    <?= _langTpl('published_on') ?>
+                    <time datetime="<?= date('c', $date) ?>"><?= date('Y-n-j H:i', $date) ?></time>
+                </span>
+                <span class="log-meta-item">
+                    <span class="iconfont icon-view"></span>
+                    <?= _langTpl('views') ?><?= $views ?>
+                </span>
+                <span class="log-meta-item"><?php blog_sort($sortid) ?></span>
+            </div>
+            <div class="log-meta-right"><?php editflg($logid, $author) ?></div>
+        </div>
+    </header>
     <hr class="bottom-5" />
     <div class="markdown" id="emlogEchoLog"><?= $log_content ?></div>
     <p class="top-5"><?php blog_tag($logid) ?></p>
