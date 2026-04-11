89372c0e76a2ad59f51090af639854536d7620d6
snow <4344235+emlog@users.noreply.github.com>
2026-04-12 07:23:00 +0800

    refactor(page template): restructure page layout for improved semantics

--- content/templates/default/page.php
+++ content/templates/default/page.php
@@ -5,20 +5,18 @@
  */
 defined('EMLOG_ROOT') || exit('access denied!');
 ?>
-<article class="container blog-container">
-    <div class="row">
-        <div class="column-big log-con " id="page">
-            <h1 class="page-title"><?= $log_title ?></h1>
-            <div class="markdown">
-                <?= $log_content ?>
-            </div>
-            <?php blog_comments_post($logid, $ckname, $ckmail, $ckurl, $verifyCode, $allow_remark) ?>
-            <?php blog_comments($comments, $comnum) ?>
-        </div>
-        <?php
-        include View::getView('side');
-        ?>
-    </div>
+<article class="container log-con blog-container">
+    <header class="log-header">
+        <h1 class="log-title"><?php topflg($top) ?><?= $log_title ?></h1>
+    </header>
+    <hr class="bottom-5" />
+    <div class="markdown" id="emlogEchoLog"><?= $log_content ?></div>
+
+    <?php doAction('log_related', $logData) ?>
+
+    <?php blog_comments_post($logid, $ckname, $ckmail, $ckurl, $verifyCode, $allow_remark) ?>
+    <?php blog_comments($comments, $comnum) ?>
+
+    <div style="clear:both;"></div>
 </article>
-<?php
-include View::getView('footer');
+<?php include View::getView('footer');
