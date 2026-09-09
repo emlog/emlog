b9f6f3e59a144bf0b29f242538757442bf51d915
snow <4344235+emlog@users.noreply.github.com>
2026-09-09 10:48:48 +0800

    fix: correct callback logic in tips plugin to prevent unexpected execution errors

--- content/plugins/tips/tips_callback.php
+++ content/plugins/tips/tips_callback.php
@@ -2,17 +2,19 @@
 defined('EMLOG_ROOT') || exit('access denied!');
 
 // 开启插件时执行该函数
-function callback_init() {
+function callback_init()
+{
     // do something
 }
 
 // 删除插件时执行该函数
-function callback_rm() {
-    $plugin_storage = Storage::getInstance('tips');
-    $plugin_storage->deleteAllName('YES'); // 删除时清理插件的设置信息
+function callback_rm()
+{
+    // 卸载插件时删除插件创建的表，文件
 }
 
 // 更新插件时执行该函数
-function callback_up() {
-    // do something
+function callback_up()
+{
+    // 升级时更新数据库表结构等操作
 }
